d0ec54f84600d0f885483e1eb4bb3513c98595a1
linushof <linus.hof@tum.de>
2022-08-18 18:14:00 +0200

    rename parameter variables and prospects (roundwise integration)

--- code/model_implementation_roundwise.R
+++ code/model_implementation_roundwise.R
@@ -1,24 +1,24 @@
 pacman::p_load(tidyverse)
-source("R/fun_compute_cumulative_stats.R") # call functions for computing cumulative stats
+source("R/helper_functions/fun_compute_cumulative_stats.R") # call functions for computing cumulative stats
 
 # test set
-gambles <- read_csv("data/choice_problems.csv")
-n_agents <- 100
+choice_problems <- read_rds("data/choice_problems.rds")
+n_agents <- 100 # number of synthetic agents
 
 # simulation parameters
-theta <- expand_grid(s = seq(-.5, .4, .1), # probability increment added to unbiased sampling probability of p = .5
-                     boundary = c("absolute", "relative"), # boundary type
-                     a = seq(1, 5, 1)) # boundaries (number of required round-wins)
+param <- expand_grid(psi = seq(-.5, .4, .1), # probability increment added to unbiased sampling probability of p = .5
+                     threshold = c("absolute", "relative"), # threshold type
+                     theta = seq(1, 5, 1)) # thresholds
 
 # simulation
-## for each parameter combination (rows of theta), all gambles are played by all agents
-## 100 (parameter combinations) x 60 (gambles) x 100 (agents) = 600.000 trials
+## for each parameter combination (rows of param), all choice problems are played by all agents
+## 100 (parameter combinations) x 60 (problems) x 100 (agents) = 600.000 trials
 
 set.seed(56221)
-param_list <- vector("list", length(nrow(theta)))
-for (set in seq_len(nrow(theta))) {  # loop over parameter combinations
-  gamble_list <- vector("list", length(nrow(gambles)))
-  for (gamble in seq_len(nrow(gambles))) { # loop over gambles
+param_list <- vector("list", length(nrow(param)))
+for (set in seq_len(nrow(param))) {  # loop over parameter combinations
+  problem_list <- vector("list", length(nrow(choice_problems)))
+  for (problem in seq_len(nrow(choice_problems))) { # loop over gambles
     agents_list <- vector("list", n_agents)
     for (agent in seq_along(1:n_agents)){ # loop over agents
 
@@ -26,97 +26,97 @@ for (set in seq_len(nrow(theta))) {  # loop over parameter combinations
 
       fd <- tibble() # frequency distribution of sampled outcomes
       p <- .5  # no attention bias
-      s <- 0  # no switching at process initiation
-      init <- sample(c("a", "b"), size = 1, prob = c(p + s, p - s)) # prospect attended first
+      psi <- 0  # no switching at process initiation
+      init <- sample(c("r", "s"), size = 1, prob = c(p + psi, p - psi)) # prospect attended first
       attend <- init
       round <- 1
       boundary_reached <- FALSE
 
-      # sampling of outcomes from prospects A and B continues until boundary is reached
+      # sampling of outcomes from risky (r) and safe (s) prospect continues until boundary is reached
 
       while(boundary_reached == FALSE) {
 
         # prospects are compared round-wise
-        ## a round consists of an uninterrupted sequence of sampled outcomes each from A and B
+        ## a round consists of an uninterrupted sequence of sampled outcomes each from r and s
         ## prospects with a higher mean of sampled outcomes within a round earn a round-win
 
         smpl_round <- tibble()
         while(attend == init) {# sequence of single samples from prospect attended first
 
-          # draw single sample from either A or B
+          # draw single sample from either r or s
 
-          if(attend == "a") {
-            single_smpl <- gambles[gamble, ] %>%
+          if(attend == "r") {
+            single_smpl <- choice_problems[problem, ] %>%
               mutate(round = round,
                      attended = attend,
-                     A = sample(x = c(a_o1, a_o2), size = 1, prob = c(a_p1, 1-a_p1)),
-                     B = NA)
-            s <- theta[[set, "s"]] # to update the probability of sampling from A again
+                     r = sample(x = c(x_low, x_high), size = 1, prob = c(p_x_low, p_x_high)),
+                     s = NA)
+            psi <- param[[set, "psi"]] # to update the probability of sampling from r again
           } else {
-            single_smpl <- gambles[gamble, ] %>%
+            single_smpl <- choice_problems[problem, ] %>%
               mutate(round = round,
                      attended = attend,
-                     A = NA,
-                     B = b_o1)
-            s <- -1*theta[[set, "s"]] # to update the probability of of sampling from B again
+                     r = NA,
+                     s = safe)
+            psi <- -1*theta[[set, "psi"]] # to update the probability of of sampling from s again
           }
           smpl_round <- bind_rows(smpl_round, single_smpl)
-          attend <- sample(c("a", "b"), size = 1, prob = c(p + s, p - s))
+          attend <- sample(c("r", "s"), size = 1, prob = c(p + psi, p - psi))
         }
 
         while(attend != init) { # sequence of single samples from prospect attended second
 
-          # draw single sample from either A or B
+          # draw single sample from either r or s
 
-          if(attend == "a") {
-            single_smpl <- gambles[gamble, ] %>%
+          if(attend == "r") {
+            single_smpl <- choice_problems[problem, ] %>%
               mutate(round = round,
                      attended = attend,
-                     A = sample(x = c(a_o1, a_o2), size = 1, prob = c(a_p1, 1-a_p1)),
-                     B = NA)
-            s <- theta[[set, "s"]]
+                     r = sample(x = c(x_low, x_high), size = 1, prob = c(p_x_low, p_x_high)),
+                     s = NA)
+            psi <- theta[[set, "psi"]]
           } else {
-            single_smpl <- gambles[gamble, ] %>%
+            single_smpl <- choice_problems[problem, ] %>%
               mutate(round = round,
                      attended = attend,
-                     A = NA,
-                     B = b_o1)
-            s <- -1*theta[[set, "s"]]
+                     r = NA,
+                     s = safe)
+            psi <- -1*theta[[set, "psi"]]
           }
           smpl_round <- bind_rows(smpl_round, single_smpl)
-          attend <- sample(c("a", "b"), size = 1, prob = c(p + s, p - s))
+          attend <- sample(c("r", "s"), size = 1, prob = c(p + psi, p - psi))
         }
 
-        # compare means over sampled outcomes from A and B
+        # compare means over sampled outcomes from r and s
         # assign round-win
 
         smpl_round <- smpl_round %>%
-          mutate(A_rmean = cummean2(A, na.rm = TRUE),
-                 B_rmean = cummean2(B, na.rm = TRUE),
-                 rdiff = A_rmean - B_rmean)
-        smpl_round[[nrow(smpl_round), "A_win"]] <- case_when(smpl_round[[nrow(smpl_round), "rdiff"]] > 0 ~ 1,
+          mutate(r_rmean = cummean2(r, na.rm = TRUE),
+                 s_rmean = cummean2(s, na.rm = TRUE),
+                 rdiff = r_rmean - s_rmean)
+        smpl_round[[nrow(smpl_round), "r_win"]] <- case_when(smpl_round[[nrow(smpl_round), "rdiff"]] > 0 ~ 1,
                                                              smpl_round[[nrow(smpl_round), "rdiff"]] <= 0 ~ 0)
-        smpl_round[[nrow(smpl_round), "B_win"]] <-  case_when(smpl_round[[nrow(smpl_round), "rdiff"]] >= 0 ~ 0,
+        smpl_round[[nrow(smpl_round), "s_win"]] <-  case_when(smpl_round[[nrow(smpl_round), "rdiff"]] >= 0 ~ 0,
                                                               smpl_round[[nrow(smpl_round), "rdiff"]] < 0 ~ 1)
 
         # add sampling round to frequency distribution of sampled outcomes
 
         fd <- bind_rows(fd, smpl_round)
-        fd[[nrow(fd), "A_sum"]] <- sum(fd[["A_win"]], na.rm = TRUE)
-        fd[[nrow(fd), "B_sum"]] <- sum(fd[["B_win"]], na.rm = TRUE)
+        fd[[nrow(fd), "r_sum"]] <- sum(fd[["r_win"]], na.rm = TRUE)
+        fd[[nrow(fd), "s_sum"]] <- sum(fd[["s_win"]], na.rm = TRUE)
 
         # evaluate accumulated evidence (as round-wins) over fd
         # evidence is either compared against the absolute or relative boundary
 
         if(theta[[set, "boundary"]] == "absolute") {
           fd <- fd %>%
-            mutate(choice = case_when(A_sum >= theta[[set, "a"]] ~ "A",
-                                      B_sum >= theta[[set, "a"]] ~ "B"))
+            mutate(choice = case_when(r_sum >= param[[set, "theta"]] ~ "r",
+                                      s_sum >= param[[set, "theta"]] ~ "s"))
         } else {
-          fd[[nrow(fd), "diff"]] <- fd[[nrow(fd), "A_sum"]] - fd[[nrow(fd), "B_sum"]]
+          fd[[nrow(fd), "diff"]] <- fd[[nrow(fd), "r_sum"]] - fd[[nrow(fd), "s_sum"]]
           fd <- fd %>%
-            mutate(choice = case_when(diff >= theta[[set, "a"]] ~ "A",
-                                      diff <= -1*theta[[set, "a"]] ~ "B"))
+            mutate(choice = case_when(diff >= param[[set, "theta"]] ~ "r",
+                                      diff <= -1*param[[set, "theta"]] ~ "s"))
         }
 
         # if boundary is not reached, start new sampling round
@@ -130,10 +130,12 @@ for (set in seq_len(nrow(theta))) {  # loop over parameter combinations
       agents_list[[agent]] <- expand_grid(agent, fd)
     }
     all_agents <- agents_list %>% map_dfr(as.list)
-    gamble_list[[gamble]] <- expand_grid(gamble, all_agents)
+    problem_list[[problem]] <- expand_grid(problem, all_agents)
   }
-  all_gambles <- gamble_list %>% map_dfr(as.list)
-  param_list[[set]] <- expand_grid(theta[set, ], all_gambles)
+  all_problems <- problem_list %>% map_dfr(as.list)
+  param_list[[set]] <- expand_grid(param[set, ], all_problems)
 }
-simulation <- param_list %>% map_dfr(as.list)
-write_csv(simulation, "data/simulation.csv")
+simulation_roundwise <- param_list %>% map_dfr(as.list)
+
+# safe as compressed data file
+write_rds(simulation_roundwise, "data/simulation_roundwise.rds.bz2", compress = "bz2")

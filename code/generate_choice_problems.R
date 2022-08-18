6c6f7502a76a7b4e3c33b0c85e64c34ff396c6f5
linushof <linus.hof@tum.de>
2022-08-18 13:20:14 +0200

    change variable names in choice problems (alignment with names in manuscript)

--- code/generate_choice_problems.R
+++ code/generate_choice_problems.R
@@ -1,24 +1,24 @@
 pacman::p_load(tidyverse)
-source("code/fun_generate_choice_problems.R") # call function for generating 2-outcome gambles
+source("code/fun_generate_choice_problems.R") # call function for generating choice problems
 
-# generate 10,000 safe-risky gambles
+# generate 10,000 safe-risky choice problems
 
 set.seed(349)
-initial_set <- generate_gambles(n = 10000, safe = TRUE, lower = 0, upper = 20)
-initial_set <- initial_set %>% mutate(rare = case_when(a_p1 >= .2 & a_p1 <= .8 ~ "none",
-                                                     a_p1 < .2 ~ "unattractive", # a_o1 is the smaller outcome
-                                                     a_p1 > .8 ~ "attractive"))
+initial_set <- generate_choice_problems(n = 10000, lower = 0, upper = 20)
+initial_set <- initial_set %>% mutate(rare = case_when(p_x_low >= .2 & p_x_low <= .8 ~ "none",
+                                                       p_x_low < .2 ~ "unattractive",
+                                                       p_x_low > .8 ~ "attractive"))
 
-# select subset of 60 gambles
-# stratified sampling of 20 gambles with no/attractive/unattractive rare event
+# select subset of 60 choice problems
+# stratified sampling of 20 choice problems with no/attractive/unattractive rare outcome
 
 test_set <- tibble()
 
 set.seed(735)
 for(i in unique(initial_set$rare)) {
-  type <- initial_set %>% filter(rare == i) # type of rare event
-  smpl <- sample(seq_len(nrow(type)), size = 20) # random sample of 20 rows/gambles
+  type <- initial_set %>% filter(rare == i) # type of rare rare outcome
+  smpl <- sample(seq_len(nrow(type)), size = 20) # random sample of 20 choice problems
   test_set <- bind_rows(test_set, type[smpl, ])
 }
 
-write_csv(test_set, "data/choice_problems.csv")
+write_rds(test_set, "data/choice_problems.rds")
--- code/helper_functions/fun_generate_choice_problems.R
+++ code/helper_functions/fun_generate_choice_problems.R
@@ -1,77 +1,47 @@
 pacman::p_load(tidyverse)
 
-# Function for generating 2-outcome gambles
-## n: number of gambles
-## safe: gamble typ; TRUE = safe vs. risky option; FALSE = risky options only
-## lower, upper: lower and upper boundary of outcome range
+# Function for generating choice problems between a risky and safe prospect
+## n: number of choice problems
+## lower: lower boundary of the outcome range
+## upper: upper boundary of the outcome range
 
-generate_gambles <- function(n, safe = TRUE, lower, upper) {
+generate_choice_problems <- function(n, lower, upper) {
 
-  # define a function for computing expected values
+  # define function for computing a prospect's expected value (ev)
+  ## p_x_low: probability of the smaller risky outcome
+  ## x_low: smaller risky outcome
+  ## x_high: higher risky outcome
 
-  ev <- function(p1, o1, o2 = 0) {
-    round(p1 * o1 + (1-p1) * o2, digits = 2)
+  ev <- function(p_x_low, x_low, x_high) {
+    round(p_x_low * x_low + (1-p_x_low) * x_high, digits = 2)
   }
 
   output <- vector("list", n)
 
-  # safe vs. risky gambles
+  # for each choice problem, randomly draw outcomes and probabilities
+  
+  output %>%
+    map(tibble, # create tibble for each choice problem
+        "names" = c("x_low", "safe", "x_high", "p_x_low"),
+        "values" = c(runif(3, min = lower, max = upper) %>% # draw 3 outcomes
+                       round(2) %>% 
+                        sort(), # sort and assign outcomes to omit dominance, i.e., x_low < safe < x_high
+                     runif(1, min = .01, max = .99) %>% # probability of x_low
+                       round(2))
+        ) %>%
 
-  if(safe == TRUE) {
-
-    # for each gamble, randomly draw outcomes and probabilities
-
-    output %>%
-      map(tibble, # create tibble for each gamble
-          "names" = c("a_o1", "b_o1", "a_o2", "a_p1"), # randomly generated values
-          "values" = c(runif(3, min = lower, max = upper) %>%  # outcomes
-                         round(2) %>%
-                         sort(),
-                       runif(1, min = .01, max = .99) %>% # probabilities
-                         round(2))
-          ) %>%
-
-      # each gamble should be presented in a single row
+      # each choice problem should be presented in a single row:
 
       map(pivot_wider, names_from = "names", values_from = "values") %>%
       map_dfr(as.list) %>%
 
-      # add gamble features that are determined by the random samples from above
-      # compute and compare expected values
+      # add prospect features and compute and compare expected values
 
-      mutate(a_p2 = round(1-a_p1,2),
-             b_o2 = 0,
-             b_p1 = 1,
-             b_p2 = 0,
-             a_ev = ev(a_p1, a_o1, a_o2),
-             b_ev = ev(b_p1, b_o1, b_o2),
-             ev_diff = round(a_ev - b_ev, 2),
-             ev_ratio = round(a_ev/b_ev, 2)
+      mutate(p_x_high = round(1-p_x_low, 2),
+             p_safe = 1,
+             x_ev = ev(p_x_low, x_low, x_high),
+             ev_diff = round(x_ev - safe, 2),
+             ev_ratio = round(x_ev/safe, 2)
       ) %>%
-      select(a_p1, a_o1, a_p2, a_o2, b_p1, b_o1, b_p2, b_o2, a_ev, b_ev, ev_diff, ev_ratio) # sort features
-
-  } else {
-
-    # risky vs. risky gambles
-
-    output %>%
-      map(tibble,
-          "names" = c("a_o1", "b_o1", "a_o2", "b_o2", "a_p1", "b_p1"),
-          "values" = c(runif(3, min = lower, max = upper) %>% round(2) %>%
-                         sort(), # prevent dominance: a_o1 < b_o1 < a_o2
-                       runif(1, min = lower, max = upper) %>% round(2), # b_o2
-                       runif(2, min = .01, max = .99)
-          )
-      ) %>%
-      map(pivot_wider, names_from = "names", values_from = "values") %>%
-      map_dfr(as.list) %>%
-      mutate(a_p2 = 1-a_p1,
-             b_p2 = 1-b_p1,
-             a_ev = ev(a_p1, a_o1, a_o2),
-             b_ev = ev(b_p1, b_o1, b_o2),
-             ev_diff = round(a_ev - b_ev, 2),
-             ev_ratio = round(a_ev/b_ev, 2)
-      ) %>%
-      select(a_p1, a_o1, a_p2, a_o2, b_p1, b_o1, b_p2, b_o2, a_ev, b_ev, ev_diff, ev_ratio)
-  }
+      select(p_x_low, x_low, p_x_high, x_high, p_safe, x_ev, safe, ev_diff, ev_ratio) # sort features
 }

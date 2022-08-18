f126310563020345cbf0fe94f0b6e19cf8b7a485
linushof <linus.hof@tum.de>
2022-08-18 16:25:52 +0200

    Rename parameter variables and prospects

--- code/compress-data.R
+++ code/compress-data.R
@@ -1,38 +1,26 @@
-library(tidyverse)
-
-# choice data
-
-## read
-cols_choices <- list(.default = col_double(),
-                     boundary = col_factor(),
-                     gamble = col_factor(),
-                     rare = col_factor(),
-                     agent = col_factor(),
-                     choice = col_factor())
-
-choices_roundwise <- read_csv("data/choices_roundwise.csv", col_types = cols_choices)
-choices_summary <- read_csv("data/choices_summary.csv", col_types = cols_choices)
-
-## save compressed data
-write_rds(choices_roundwise, file = "data/compressed/choices_roundwise.rds.bz2", compress = "bz2")
-write_rds(choices_summary, file = "data/compressed/choices_summary.rds.bz2", compress = "bz2")
-
-
 # simulation data
 
-## read
+simulation_summary <- read_rds("data/compressed/simulation_summary.rds.bz2")
 
-cols <- list(.default = col_double(),
-             boundary = col_factor(),
-             gamble = col_factor(),
-             agent = col_factor(),
-             rare = col_factor(),
-             attended = col_factor(),
-             choice = col_factor())
-simulation_roundwise <- read_csv("data/simulation_roundwise.csv", col_types = cols)
-simulation_summary <- read_csv("data/simulation_summary.csv", col_types = cols)
-
+simulation_summary <- simulation_summary %>% 
+  rename(psi = s,
+         threshold = boundary,
+         theta = a, 
+         problem = gamble,
+         p_x_low = a_p1,
+         x_low = a_o1,
+         p_x_high = a_p2,
+         x_high = a_o2,
+         p_safe = b_p1,
+         safe = b_o1,
+         x_ev = a_ev,
+         r = A,
+         s = B,
+         r_sum = A_sum,
+         s_sum = B_sum) %>% 
+  mutate(attended = if_else(attended == "a", "r", "s"), 
+         choice = if_else(choice == "A", "r", "s")) %>% 
+  select(-c(b_p2, b_o2))
+                             
 ## save compressed data
-write_rds(simulation_roundwise, file = "data/compressed/simulation_roundwise.rds.bz2", compress = "bz2")
 write_rds(simulation_summary, file = "data/compressed/simulation_summary.rds.bz2", compress = "bz2")
-
--- code/model_implementation_summary.R
+++ code/model_implementation_summary.R
@@ -2,23 +2,23 @@ pacman::p_load(tidyverse)
 source("R/fun_cumulative_stats.R") # call functions for computing cumulative stats
 
 # test set
-gambles <- read_csv("data/gambles/sr_subset.csv")
-n_agents <- 100
+choice_problems <- read_rds("data/choice_problems.rds")
+n_agents <- 100 # number of synthetic agents
 
 # simulation parameters
-theta <- expand_grid(s = seq(-.5, .4, .1), # probability increment added to unbiased sampling probability of p = .5
-                     boundary = c("absolute", "relative"), # boundary type
-                     a = seq(15, 75, 15)) # boundaries
+param <- expand_grid(psi = seq(-.5, .4, .1), # probability increment added to unbiased sampling probability of p = .5
+                     threshold = c("absolute", "relative"), # threshold type
+                     theta = seq(15, 75, 15)) # thresholds
 
 # simulation
-## for each parameter combination (rows of theta), all gambles are played by all agents
-## 100 (parameter combinations) x 60 (gambles) x 100 (agents) = 600.000 trials
+## for each parameter combination (rows of param), all choice problems are played by all agents
+## 100 (parameter combinations) x 60 (problems) x 100 (agents) = 600.000 trials
 
 set.seed(19543)
-param_list <- vector("list", length(nrow(theta)))
-for (set in seq_len(nrow(theta))) { # loop over parameter combinations
-  gamble_list <- vector("list", length(nrow(gambles)))
-  for (gamble in seq_len(nrow(gambles))) { # loop over gambles
+param_list <- vector("list", length(nrow(param)))
+for (set in seq_len(nrow(param))) { # loop over parameter combinations
+  problem_list <- vector("list", length(nrow(choice_problems)))
+  for (problem in seq_len(nrow(choice_problems))) { # loop over choice problems
     agents_list <- vector("list", n_agents)
     for (agent in seq_along(1:n_agents)){ # loop over agents
 
@@ -26,66 +26,68 @@ for (set in seq_len(nrow(theta))) { # loop over parameter combinations
 
       fd <- tibble() # frequency distribution of sampled outcomes
       p <- .5  # no attention bias
-      s <- 0  # no switching at process initiation
-      init <- sample(c("a", "b"), size = 1, prob = c(p + s, p - s)) # prospect attended first
+      psi <- 0  # no switching at process initiation
+      init <- sample(c("r", "s"), size = 1, prob = c(p + psi, p - psi)) # prospect attended first
       attend <- init
       boundary_reached <- FALSE
 
-      # sampling of outcomes from prospects A and B continues until boundary is reached
+      # sampling of outcomes from risky (r) and safe (s) prospect continues until boundary is reached
 
       while(boundary_reached == FALSE) {
+        
+        # draw single sample from either r or s
 
-        # draw single sample from either A or B
-
-        if(attend == "a") {
-          single_smpl <- gambles[gamble, ] %>%
+        if(attend == "r") {
+          single_smpl <- choice_problems[problem, ] %>%
             mutate(attended = attend,
-                   A = sample(x = c(a_o1, a_o2), size = 1, prob = c(a_p1, 1-a_p1)),
-                   B = NA)
-          s <- theta[[set, "s"]] # to update the probability of sampling from A again
+                   r = sample(x = c(x_low, x_high), size = 1, prob = c(p_x_low, p_x_high)),
+                   s = NA)
+          psi <- param[[set, "psi"]] # to update the probability of sampling from r again
         } else {
-          single_smpl <- gambles[gamble, ] %>%
+          single_smpl <- choice_problems[problem, ] %>%
             mutate(attended = attend,
-                   A = NA,
-                   B = b_o1)
-          s <- -1*theta[[set, "s"]] # to update the probability of sampling from B again
+                   r = NA,
+                   s = safe)
+          psi <- -1*param[[set, "psi"]] # to update the probability of sampling from s again
         }
 
         # add single sample to frequency distribution of sampled outcomes
 
         fd <- bind_rows(fd, single_smpl) %>%
-          mutate(A_sum = cumsum2(A, na.rm = TRUE),
-                 B_sum = cumsum2(B, na.rm = TRUE))
+          mutate(r_sum = cumsum2(r, na.rm = TRUE),
+                 s_sum = cumsum2(s, na.rm = TRUE))
 
         # evaluate accumulated evidence over fd
         # evidence is either compared against the absolute or relative boundary
 
-        if(theta[[set, "boundary"]] == "absolute") {
+        if(param[[set, "threshold"]] == "absolute") {
           fd <- fd %>%
-            mutate(choice = case_when(A_sum >= theta[[set, "a"]] ~ "A",
-                                      B_sum >= theta[[set, "a"]] ~ "B"))
+            mutate(choice = case_when(r_sum >= param[[set, "theta"]] ~ "r",
+                                      s_sum >= param[[set, "theta"]] ~ "s"))
         } else {
           fd <- fd %>%
-            mutate(diff = round(A_sum - B_sum, 2),
-                   choice = case_when(diff >= theta[[set, "a"]] ~ "A",
-                                      diff <= -1*theta[[set, "a"]] ~ "B"))
+            mutate(diff = round(r_sum - s_sum, 2),
+                   choice = case_when(diff >= param[[set, "theta"]] ~ "r",
+                                      diff <= -1*param[[set, "theta"]] ~ "s"))
         }
 
-        # if boundary is not reached, draw new sample from A (B) according to s
+        # if boundary is not reached, draw new sample from r (s) according to psi
 
         if(is.na(fd[[nrow(fd), "choice"]]) == FALSE) {
           boundary_reached <- TRUE
         } else {
-          attend <- sample(c("a", "b"), size = 1, prob = c(p + s, p - s))
+          attend <- sample(c("r", "s"), size = 1, prob = c(p + psi, p - psi))
         }
       }
       agents_list[[agent]] <- expand_grid(agent, fd)
     }
     all_agents <- agents_list %>% map_dfr(as.list)
-    gamble_list[[gamble]] <- expand_grid(gamble, all_agents)
+    problem_list[[problem]] <- expand_grid(problem, all_agents)
   }
-  all_gambles <- gamble_list %>% map_dfr(as.list)
-  param_list[[set]] <- expand_grid(theta[set, ], all_gambles)
+  all_problems <- problem_list %>% map_dfr(as.list)
+  param_list[[set]] <- expand_grid(param[set, ], all_problems)
 }
-sim_comprehensive <- param_list %>% map_dfr(as.list)
-write_csv(sim_comprehensive, "supplements/comprehensive/data/simulation_comprehensive.csv")
+simulation_summary <- param_list %>% map_dfr(as.list)
+
+# safe as compressed data file
+write_rds(simulation_summary, "data/simulation_summary.rds.bz2", compress = "bz2")

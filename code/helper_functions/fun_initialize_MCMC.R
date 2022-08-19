c6c6f08ebadaed74d1f95824f5073ef88181d923
linushof <linus.hof@tum.de>
2022-08-19 15:02:03 +0200

    Remove script for initializing MCMC sampling in JAGS

--- code/helper_functions/fun_initialize_MCMC.R
+++
@@ -1,4 +0,0 @@
-inits_MCMC <- function(){list("alpha" = 1,
-                              "gamma.pre" = .5,
-                              "delta.pre" = .5,
-                              "rho.pre" = 0)}

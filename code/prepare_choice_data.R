6202cfcdf6fe5a76f69eb9b65d75b0852a5e0690
linushof <linus.hof@tum.de>
2022-08-19 14:53:55 +0200

    use common script for choice data preparation

---
+++ code/prepare_choice_data.R
@@ -0,0 +1,45 @@
+pacman::p_load(tidyverse)
+
+# summary model
+simulation_summary <- read_rds("data/simulation_summary.rds.bz2")
+
+## transform data to obtain trial summaries
+choice_data_summary <- simulation_summary %>%
+  group_by(psi, threshold, theta, problem, agent) %>%
+  mutate(n_sample = n(), # total number of single samples
+         n_r = n_sample - sum(is.na(r)), # number of single samples drawn from risky option
+         ep_r_high = round(sum(if_else(r == r_high, 1, 0), na.rm = TRUE)/n_r, 2), # experienced probability of higher risky outcome
+         ep_r_low = round(1 - ep_r_high, 2), # experienced probability of lower risky outcome
+         mean_r = round(mean(r, na.rm = TRUE), 2)) %>% # sampled mean risky prospect
+  ungroup() %>%
+  filter(!is.na(choice)) # discard single samples
+
+## tidy data 
+choice_data_summary <- choice_data_summary %>% 
+  mutate(psi = 1-(psi+.5)) %>% # to interpret psi as switching probability
+  select(psi:problem, rare, p_r_low:ev_ratio, agent, n_sample, n_r, ep_r_low, ep_r_high, mean_r, r_sum, s_sum, diff, choice)
+
+# roundwise model
+simulation_roundwise <- read_rds("data/simulation_roundwise.rds.bz2")
+
+## transform data to obtain trial summaries
+choice_data_roundwise <- simulation_roundwise %>% 
+  group_by(psi, threshold, theta, problem, agent) %>% 
+    mutate(n_sample = n(),
+           n_r = n_sample - sum(is.na(r)),
+           ep_r_high = round(sum(if_else(r == r_high, 1, 0), na.rm = TRUE)/n_r, 2),
+           ep_r_low = round(1 - ep_r_high, 2),
+           mean_r = round(mean(r, na.rm = TRUE), 2)) %>%
+  ungroup() %>%
+  filter(!is.na(choice))
+    
+
+## tidy data
+choice_data_roundwise <- choice_data_roundwise %>% 
+  mutate(psi = 1-(psi+.5)) %>%
+  select(psi:problem, rare, p_r_low:ev_ratio, agent, n_sample, n_r, ep_r_low, ep_r_high, mean_r, r_sum, s_sum, diff, choice)
+
+
+# join data sets and save as compressed data file
+choice_data <- bind_rows(choice_data_summary, choice_data_summary)
+write_rds(choice_data, "data/choice_data.rds.bz2", compress = "bz2")

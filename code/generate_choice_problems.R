19da9bfa6c7060b0d64bcd7475a532788a4ad58c
linushof <linus.hof@tum.de>
2023-04-20 15:31:30 +0200

    tidy code

--- code/generate_choice_problems.R
+++ code/generate_choice_problems.R
@@ -1,18 +1,21 @@
 pacman::p_load(tidyverse)
 source("code/helper_functions/fun_generate_choice_problems.R") # call function for generating choice problems
 
-# generate 10,000 safe-risky choice problems
+# generate 10,000 choice problems
 
 set.seed(349)
 initial_set <- generate_choice_problems(n = 10000, lower = 0, upper = 20)
-initial_set <- initial_set %>% mutate(rare = case_when(p_r_low >= .2 & p_r_low <= .8 ~ "none",
-                                                       p_r_low < .2 ~ "unattractive",
-                                                       p_r_low > .8 ~ "attractive"))
 
-# select subset of 60 choice problems
-# stratified sampling of 20 choice problems with no/attractive/unattractive rare outcome
+# sample test set of 60 choice problems
 
 test_set <- tibble()
+
+# stratified sampling of 20 choice problems with no/attractive/unattractive rare outcome
+initial_set <- initial_set %>%
+  mutate(rare = case_when(p_r_low >= .2 & p_r_low <= .8 ~ "none" ,
+                          p_r_low < .2 ~ "unattractive" ,
+                          p_r_low > .8 ~ "attractive"))
+
 
 set.seed(735)
 for(i in unique(initial_set$rare)) {

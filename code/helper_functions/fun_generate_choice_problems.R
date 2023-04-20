d100de84874e804aa6d8024176ccfae8d6f21d79
linushof <linus.hof@tum.de>
2023-04-20 15:02:41 +0200

    tidy code, use bind_rows instead of map_dfr

--- code/helper_functions/fun_generate_choice_problems.R
+++ code/helper_functions/fun_generate_choice_problems.R
@@ -1,47 +1,40 @@
+# load pkgs
 pacman::p_load(tidyverse)
 
-# Function for generating choice problems between a risky and safe prospect
-## n: number of choice problems
-## lower: lower boundary of the outcome range
-## upper: upper boundary of the outcome range
+# Function for computing an option's expected value 
+
+ev <- function(p_r_low, r_low, r_high) {
+  # p_r_low: probability of the smaller risky outcome
+  # r_low: smaller risky outcome
+  # r_high: higher risky outcome
+  
+  round(p_r_low * r_low + (1-p_r_low) * r_high, digits = 2)
+}
+
+# Function for generating choice problems consisting of a risky and a safe option
 
 generate_choice_problems <- function(n, lower, upper) {
+  # n: number of choice problems
+  # lower: lower boundary of the outcome range
+  # upper: upper boundary of the outcome range
 
-  # define function for computing a prospect's expected value (ev)
-  ## p_r_low: probability of the smaller risky outcome
-  ## r_low: smaller risky outcome
-  ## r_high: higher risky outcome
-
-  ev <- function(p_r_low, r_low, r_high) {
-    round(p_r_low * r_low + (1-p_r_low) * r_high, digits = 2)
-  }
-
-  output <- vector("list", n)
-
-  # for each choice problem, randomly draw outcomes and probabilities
-  
-  output %>%
-    map(tibble, # create tibble for each choice problem
-        "names" = c("r_low", "safe", "r_high", "p_r_low"),
-        "values" = c(runif(3, min = lower, max = upper) %>% # draw 3 outcomes
-                       round(2) %>% 
-                        sort(), # sort and assign outcomes to omit dominance, i.e., r_low < safe < r_high
-                     runif(1, min = .01, max = .99) %>% # probability of r_low
-                       round(2))
-        ) %>%
-
-      # each choice problem should be presented in a single row:
-
-      map(pivot_wider, names_from = "names", values_from = "values") %>%
-      map_dfr(as.list) %>%
-
-      # add prospect features and compute and compare expected values
-
-      mutate(p_r_high = round(1-p_r_low, 2),
-             p_safe = 1,
-             r_ev = ev(p_r_low, r_low, r_high),
-             ev_diff = round(r_ev - safe, 2),
-             ev_ratio = round(r_ev/safe, 2)
-      ) %>%
-      select(p_r_low, r_low, p_r_high, r_high, r_ev, p_safe, safe, ev_diff, ev_ratio) # sort features
+  output <- vector("list", n) # each element of the list represents 1 choice problem
+  output %>% 
+    
+    #1 generate choice problems by drawing random outcomes and probabilities
+    map(tibble , "names" = c("r_low", "safe", "r_high", "p_r_low") ,
+                 "values" = c( sort(round(runif(3, min = lower, max = upper), 2)) , # draw 3 outcomes (sort to omit dominance)
+                               round(runif(1, min = .01, max = .99),2) )) %>% # draw probability of r_low
+    
+    #2 turn list into data frame (each choice problem represents 1 row)
+    map(pivot_wider, names_from = "names", values_from = "values") %>%
+    bind_rows() %>%
+    
+    #3 add features
+    mutate(p_r_high = round(1-p_r_low, 2) ,
+           p_safe = 1 ,
+           r_ev = ev(p_r_low, r_low, r_high) ,
+           ev_diff = round(r_ev - safe, 2) ,
+           ev_ratio = round(r_ev/safe, 2)) %>%
+    select(p_r_low, r_low, p_r_high, r_high, r_ev, p_safe, safe, ev_diff, ev_ratio)
 }

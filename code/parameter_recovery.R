8daf42eff5fa37dd2410dc7c7aae3ac67ce7719f
linushof <linus.hof@tum.de>
2023-09-22 11:48:47 +0200

    plots systematicity choice behavior

--- code/parameter_recovery.R
+++ code/parameter_recovery.R
@@ -1,5 +1,5 @@
 # packages
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse, viridis)
 
 # data
 cpt <- read_rds("data/cpt_estimates.rds")
@@ -11,10 +11,17 @@ problems <- read_rds("data/choice_problems.rds")
 cases <- cpt %>% filter(model == "summary" & threshold == "relative" & parameter == "rho")
 rho_estimates <- sort(cases$mean) # means of posterior distributions of choice consistency parameter rho
 params <- expand_grid(rho = unique(round(rho_estimates, 2)) , 
-                     alpha = seq(0, 2, length.out = 5) ,
-                     gamma = seq(0, 2, length.out = 5) ,
-                     delta = seq(0, 10, length.out = 5)
+                     alpha = seq(.5, 1.5, length.out = 5) ,
+                     gamma = seq(.5, 1.5, length.out = 5) ,
+                     delta = seq(.5, 1.5, length.out = 5)
                      )
+nrow(params)
+params <- expand_grid(rho = seq(.2, 1, length.out = 5) , 
+                      alpha = seq(.5, 1.5, length.out = 5) ,
+                      gamma = seq(.5, 1.5, length.out = 5) ,
+                      delta = seq(.5, 1.5, length.out = 5)
+)
+nrow(params)
 params <- params %>% mutate(set_no = row_number()) %>% select(set_no, everything()) 
 problems <- problems %>% mutate(problem_no = row_number()) %>% select(problem_no, everything())
 sim_cases <- expand_grid(params, problems) 
@@ -44,11 +51,90 @@ predictions <- sim_cases %>% mutate(
 predictions %>% 
   ggplot(aes(x = V_diff, y = safe_perc)) + 
   geom_jitter(alpha = .05) +
-  scale_x_continuous(limits = c(-3, 3)) +
+  scale_x_continuous(limits = c(-5, 5)) +
   facet_wrap(~rho) + 
   theme_minimal()
 
+predictions %>%  
+  ggplot(aes(x = gamma, y = safe_perc)) + 
+  geom_jitter(alpha = .05) + 
+  facet_wrap(~rho) + 
+  theme_minimal()
+  
+predictions %>% 
+  mutate(EV_max_perc = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
+  ggplot(aes(x = gamma, y = EV_max_perc)) + 
+  geom_jitter(alpha = .05) + 
+  facet_wrap(~rho) + 
+  theme_minimal()
+
+
+test <- predictions %>% 
+  mutate(EV_max_perc = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
+  group_by(rho, gamma, delta, alpha) %>% 
+  summarize(mean_EV_max_perc = round( mean(EV_max_perc), 2 ))
+
+test %>% 
+  filter(alpha == 1) %>% 
+  ggplot(aes(x = delta, y = mean_EV_max_perc, group = gamma, color = gamma)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal() + 
+  scale_color_viridis()
+
+test %>% 
+  filter(alpha == 1) %>% 
+  ggplot(aes(x = gamma, y = mean_EV_max_perc, group = delta, color = delta)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal() + 
+  scale_color_viridis()
+
+test %>% 
+  filter(delta == 1 & gamma == 1) %>% 
+  ggplot(aes(x = alpha, y = mean_EV_max_perc)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal()
+
+
+test2 <- predictions %>% 
+  group_by(rho, gamma, delta, alpha) %>% 
+  summarize(mean_safe_perc = round( mean(safe_perc), 2 ))
+
+test2 %>% 
+  filter(delta == 1 & gamma == 1) %>% 
+  ggplot(aes(x = alpha, y = mean_safe_perc)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal()
+
+
+test2 %>% 
+  filter(alpha == 1) %>% 
+  ggplot(aes(x = delta, y = mean_safe_perc, group = gamma, color = gamma)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal() + 
+  scale_color_viridis()
+
+test2 %>% 
+  filter(alpha == 1) %>% 
+  ggplot(aes(x = gamma, y = mean_safe_perc, group = delta, color = delta)) + 
+  geom_point(size = 2) + 
+  geom_line(size = 1) + 
+  facet_wrap(~rho) + 
+  theme_minimal() + 
+  scale_color_viridis()
+
 ## parameter recovery 
+
+
 
 
 

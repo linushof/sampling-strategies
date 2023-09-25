14106a4eef894a5d1387e6d1c90bfd02a936bcb4
linushof <linus.hof@tum.de>
2023-09-25 14:06:12 +0200

    predictions generative CPT simulation

--- supplements/parameter_recovery.R
+++ supplements/parameter_recovery.R
@@ -11,6 +11,8 @@ problems <- read_rds("data/choice_problems.rds")
 cpt <- read_rds("data/cpt_estimates.rds")
 source("code/helper_functions/fun_initialize_MCMC.R") # call function creating initial values for MCMC
 
+# CPT parameter recovery simulation ------------------------------------------
+
 # specify parameter values for simulation (rho -> based on estimates from model fits)
 cases <- cpt %>% filter(model == "summary" & threshold == "relative" & parameter == "rho")
 rho_estimates <- sort(cases$mean)
@@ -223,124 +225,107 @@ dat %>%
 ggsave("supplements/figures/alpha2.png", width = 10, height = 18)
 
 
-# parameter recovery simulation ------------------------------------------
+
+# CPT generative simulation ---------------------------------------------------
 
 # specify parameter values for simulation
 cases <- cpt %>% filter(model == "summary" & threshold == "relative" & parameter == "rho")
 rho_estimates <- sort(cases$mean) # means of posterior distributions of choice consistency parameter rho
-params <- expand_grid(rho = unique(round(rho_estimates, 2)) , 
-                     alpha = seq(.5, 1.5, length.out = 5) ,
-                     gamma = seq(.5, 1.5, length.out = 5) ,
-                     delta = seq(.5, 1.5, length.out = 5)
+params <- expand_grid(rho.sim = unique(round(rho_estimates, 2)) , 
+                     alpha.sim = seq(.5, 1.5, length.out = 5) ,
+                     gamma.sim = seq(.5, 1.5, length.out = 5) ,
+                     delta.sim = seq(.5, 1.5, length.out = 5)
                      )
 params <- params %>% mutate(set_no = row_number()) %>% select(set_no, everything()) 
 problems <- problems %>% mutate(problem_no = row_number()) %>% select(problem_no, everything())
 sim_cases <- expand_grid(params, problems) 
 
 # simulate data 
-set.seed(210923)
+set.seed(250923)
 predictions <- sim_cases %>% mutate(
-  w_high = round( (delta * p_r_high^gamma) / ( (delta*p_r_high^gamma)+(1-p_r_high)^gamma ), 2) , 
+  w_high = round( (delta.sim * p_r_high^gamma.sim) / ( (delta.sim*p_r_high^gamma.sim)+(1-p_r_high)^gamma.sim ), 2) , 
   w_low = 1 - w_high , 
-  v_high = r_high^alpha , 
-  v_low = r_low^alpha , 
-  v_safe = safe^alpha , 
+  v_high = r_high^alpha.sim , 
+  v_low = r_low^alpha.sim , 
+  v_safe = safe^alpha.sim , 
   V_safe = v_safe , 
   V_risky = (w_high * v_high) + (w_low * v_low) ,
-  V_safe_scaled = V_safe^(1/alpha) , 
-  V_risky_scaled = V_risky^(1/alpha) ,
+  V_safe_scaled = V_safe^(1/alpha.sim) , 
+  V_risky_scaled = V_risky^(1/alpha.sim) ,
   V_diff = V_safe_scaled-V_risky_scaled , 
-  p_safe_risky = round(1 / ( 1 + exp(-rho*V_diff) ),2) ,
+  p_safe_risky = round(1 / ( 1 + exp(-rho.sim*V_diff) ),2) ,
   N = 100 , 
   safe_total = rbinom(n=nrow(sim_cases), size=N, prob=p_safe_risky) , 
   safe_perc = safe_total/N
 )
 
-# check CPT behavior 
+# results 
 
-## sensibility of (aggregated choices) to difference in Valuation depending on choice sensitivity parameter
+## mapping CPT valuations onto choice proportions
 predictions %>% 
-  ggplot(aes(x = V_diff, y = safe_perc)) + 
+  ggplot(aes(x = V_diff, y = safe_perc, color = rho.sim)) + 
+  facet_wrap(~rho.sim, labeller = "label_both") + 
   geom_jitter(alpha = .05) +
   scale_x_continuous(limits = c(-5, 5)) +
-  facet_wrap(~rho) + 
+  scale_color_scico(palette = "acton") + 
+  labs(title = "Predicted Safe Choices as a Function CPT Valuations" , 
+       y = "Proportion of Safe Choices" , 
+       x = "CPT(Safe) - CPT(Risky)") + 
   theme_minimal()
+ggsave("supplements/figures/CPT_pred_1.png", width = 12, height = 8)
 
-predictions %>%  
-  ggplot(aes(x = gamma, y = safe_perc)) + 
-  geom_jitter(alpha = .05) + 
-  facet_wrap(~rho) + 
-  theme_minimal()
+## mapping CPT valuations onto choice proportions
 
-predictions %>% 
-  mutate(EV_max_perc = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
-  ggplot(aes(x = gamma, y = EV_max_perc)) + 
-  geom_jitter(alpha = .05) + 
-  facet_wrap(~rho) + 
-  theme_minimal()
+### gamma, delta 
 
+evmax_gamma_delta <- predictions %>% 
+  mutate(ev_max = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
+  group_by(rho.sim, gamma.sim, delta.sim) %>% 
+  summarize(mean_ev_max = round( mean(ev_max), 2 ))
 
-test <- predictions %>% 
-  mutate(EV_max_perc = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
-  group_by(rho, gamma, delta, alpha) %>% 
-  summarize(mean_EV_max_perc = round( mean(EV_max_perc), 2 ))
-
-test %>% 
-  filter(alpha == 1) %>% 
-  ggplot(aes(x = delta, y = mean_EV_max_perc, group = gamma, color = gamma)) + 
-  geom_point(size = 2) + 
+evmax_gamma_delta %>% 
+  ggplot(aes(x = gamma.sim, y = mean_ev_max, group = as.factor(delta.sim), color = as.factor(delta.sim))) +
+  facet_wrap(~rho.sim, labeller = "label_both") + 
+  geom_point(size = 3) + 
   geom_line(size = 1) + 
-  facet_wrap(~rho) + 
+  scale_color_viridis_d() + 
   theme_minimal() + 
-  scale_color_viridis()
+  labs(title = "EV Maximization as a Function of Gamma, Delta and Rho", 
+       x = "Simulated Gamma", 
+       y = "Proportions of EV Maximization", 
+       color = "Simulated\nDelta")
+ggsave("supplements/figures/CPT_pred_2.png", width = 12, height = 8)
 
-test %>% 
-  filter(alpha == 1) %>% 
-  ggplot(aes(x = gamma, y = mean_EV_max_perc, group = delta, color = delta)) + 
-  geom_point(size = 2) + 
+evmax_gamma_delta %>% 
+  ggplot(aes(x = delta.sim, y = mean_ev_max, group = as.factor(gamma.sim), color = as.factor(gamma.sim))) +
+  facet_wrap(~rho.sim, labeller = "label_both") + 
+  geom_point(size = 3) + 
   geom_line(size = 1) + 
-  facet_wrap(~rho) + 
+  scale_color_viridis_d() + 
   theme_minimal() + 
-  scale_color_viridis()
+  labs(title = "EV Maximization as a Function of Gamma, Delta and Rho", 
+       x = "Simulated Delta", 
+       y = "Proportions of EV Maximization", 
+       color = "Simulated\nGamma")
+ggsave("supplements/figures/CPT_pred_3.png", width = 12, height = 8)
 
-test %>% 
-  filter(delta == 1 & gamma == 1) %>% 
-  ggplot(aes(x = alpha, y = mean_EV_max_perc)) + 
+### alpha
+
+evmax_alpha <- predictions %>% 
+  mutate(ev_max = ifelse(ev_ratio < 0, safe_perc, 1 - safe_perc)) %>%
+  group_by(rho.sim, alpha.sim) %>% 
+  summarize(mean_ev_max = round( mean(ev_max), 2 ))
+
+evmax_alpha %>% 
+  ggplot(aes(x = alpha.sim, y = mean_ev_max)) +
+  facet_wrap(~rho.sim, labeller = "label_both") + 
   geom_point(size = 2) + 
   geom_line(size = 1) + 
-  facet_wrap(~rho) + 
-  theme_minimal()
-
-test2 <- predictions %>% 
-  group_by(rho, gamma, delta, alpha) %>% 
-  summarize(mean_safe_perc = round( mean(safe_perc), 2 ))
-
-test2 %>% 
-  filter(delta == 1 & gamma == 1) %>% 
-  ggplot(aes(x = alpha, y = mean_safe_perc)) + 
-  geom_point(size = 2) + 
-  geom_line(size = 1) + 
-  facet_wrap(~rho) + 
-  theme_minimal()
-
-test2 %>% 
-  filter(alpha == 1) %>% 
-  ggplot(aes(x = delta, y = mean_safe_perc, group = gamma, color = gamma)) + 
-  geom_point(size = 2) + 
-  geom_line(size = 1) + 
-  facet_wrap(~rho) + 
   theme_minimal() + 
-  scale_color_viridis()
-
-test2 %>% 
-  filter(alpha == 1) %>% 
-  ggplot(aes(x = gamma, y = mean_safe_perc, group = delta, color = delta)) + 
-  geom_point(size = 2) + 
-  geom_line(size = 1) + 
-  facet_wrap(~rho) + 
-  theme_minimal() + 
-  scale_color_viridis()
-
+  labs(title = "EV Maximization as a Function of Alpha and Rho", 
+       x = "Simulated Alpha", 
+       y = "Proportions of EV Maximization")
+ggsave("supplements/figures/CPT_pred_4.png", width = 12, height = 8)
 
 # posterior predictive check ---------------------------------------------------------------------
 

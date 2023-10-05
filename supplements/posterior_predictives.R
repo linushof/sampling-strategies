3186f17a49e598a0fa2dbe6bbea964653af1d3c8
linushof <linus.hof@tum.de>
2023-10-05 11:32:12 +0200

    extended posterior predictions: comparison of max rates conditional on better average option; 1 plot per comparison strategy

--- supplements/posterior_predictives.R
+++ supplements/posterior_predictives.R
@@ -1,9 +1,6 @@
 # load packages
 pacman::p_load(tidyverse , 
-               R2jags ,
-               digest , 
                viridis , 
-               ggbeeswarm, 
                scico, 
                patchwork, 
                ggpubr, 
@@ -14,7 +11,11 @@ pacman::p_load(tidyverse ,
 # load data
 cpt <- read_rds("data/cpt_estimates.rds")
 choices <- read_rds("data/choice_data.rds.bz2")
+postpred <- read_rds("supplements/posterior_predictives.rds.bz2")
 
+label_theta <- function(string) {
+  TeX(paste("$\\theta=$", string, sep = ""))
+}
 
 cpt %>% filter(parameter == "deviance")
 # largest deviances for summary model with low switching probabilities 
@@ -48,43 +49,109 @@ postpred <- ppset %>% mutate(
   choice_pp = rbinom(n=nrow(ppset), size=1, prob=p_safe_risky))
 write_rds(postpred, "supplements/posterior_predictives.rds.bz2", compress = "bz2")
 
-## results
 
-postpred <- read_rds("supplements/posterior_predictives.rds.bz2")
+## results
 
 ### prepare data 
 
-#### compute maximization rates (qualitative pattern)
+#### maximization rates
 
-##### sampling strategies
-obs_ev_exp_max <- postpred %>%
-  mutate(norm = case_when(mean_r/safe > 1 ~ 0, 
-                          mean_r/safe < 1 ~ 1)) %>% # determine option with higher sampled mean 
-  filter(!is.na(norm)) %>% # drop options without normative choice 
-  mutate(max = ifelse(norm == choice_obs, 1, 0)) %>% 
-  group_by(model, psi, threshold, theta, max) %>% 
-  summarise(n = n()) %>% 
-  mutate(rate = round(n/sum(n), 2)) %>% 
-  ungroup() %>%
-  filter(!(max == 0)) %>% 
+##### conditional on better average option (*_max_1)
+
+###### summary comparison
+
+# observed
+obs_max_1_sr <- choices %>% 
+  filter(model == "summary", threshold == "relative") %>% 
+  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
+         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
+  filter(!is.na(norm)) %>% 
+  mutate(norm = ifelse(norm == "r", "Risky Option", "Safe Option")) %>% 
+  group_by(psi, theta, norm, norm_choice) %>% 
+  summarise(n_norm = n()) %>% 
+  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
+  filter(norm_choice == 1) %>% 
   mutate(type = "Sampling Strategy")
 
-##### CPT posterior predictive
-pp_ev_exp_max <- postpred %>%
-  mutate(norm = case_when(mean_r/safe > 1 ~ 0, 
-                          mean_r/safe < 1 ~ 1)) %>% # determine option with higher sampled mean 
+# posterior predictive
+pp_max_1_sr <- postpred %>% 
+  filter(model == "summary", threshold == "relative") %>% 
+  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s") , 
+         choice_pp = ifelse(choice_pp == 0, "r", "s"), 
+         norm_choice = ifelse(choice_pp == norm, 1, 0)) %>% 
+  filter(!is.na(norm)) %>% 
+  mutate(norm = ifelse(norm == "r", "Risky Option", "Safe Option")) %>% 
+  group_by(psi, theta, norm, norm_choice) %>% 
+  summarise(n_norm = n()) %>% 
+  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
+  filter(norm_choice == 1) %>% 
+  mutate(type = "CPT Posterior Predictive")
+
+# merge
+max_1_sr <- bind_rows(obs_max_1_sr, pp_max_1_sr)
+
+###### roundwise comparison
+
+# observed
+obs_max_1_rr <- choices %>% 
+  filter(model == "roundwise", threshold == "relative") %>% 
+  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
+         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
+  filter(!is.na(norm)) %>% 
+  mutate(norm = ifelse(norm == "r", "Risky Option", "Safe Option")) %>% 
+  group_by(psi, theta, norm, norm_choice) %>% 
+  summarise(n_norm = n()) %>% 
+  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
+  filter(norm_choice == 1) %>% 
+  mutate(type = "Sampling Strategy")
+
+# posterior predictive
+pp_max_1_rr <- postpred %>% 
+  filter(model == "roundwise", threshold == "relative") %>% 
+  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s") ,
+         choice_pp = ifelse(choice_pp == 0, "r", "s") , 
+         norm_choice = ifelse(choice_pp == norm, 1, 0)) %>% 
+  filter(!is.na(norm)) %>% 
+  mutate(norm = ifelse(norm == "r", "Risky Option", "Safe Option")) %>% 
+  group_by(psi, theta, norm, norm_choice) %>% 
+  summarise(n_norm = n()) %>% 
+  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
+  filter(norm_choice == 1) %>% 
+  mutate(type = "CPT Posterior Predictive")
+
+# merge
+max_1_rr <- bind_rows(obs_max_1_rr, pp_max_1_rr)
+
+##### unconditional on better average option (*_max_2)
+
+# observed
+obs_max_2 <- choices %>%
+  mutate(norm = case_when(mean_r/safe > 1 ~ "r", 
+                          mean_r/safe < 1 ~ "s")) %>% # determine option with higher sampled mean 
   filter(!is.na(norm)) %>% # drop options without normative choice 
-  mutate(max = ifelse(norm == choice_pp, 1, 0)) %>% 
-  group_by(model, psi, threshold, theta, max) %>% 
+  mutate(norm_choice = ifelse(norm == choice, 1, 0)) %>% 
+  group_by(model, psi, threshold, theta, norm_choice) %>% 
   summarise(n = n()) %>% 
   mutate(rate = round(n/sum(n), 2)) %>% 
   ungroup() %>%
-  filter(!(max == 0)) %>% 
+  filter(!(norm_choice == 0)) %>% 
+  mutate(type = "Sampling Strategy")
+
+pp_max_2 <- postpred %>%
+  mutate(norm = case_when(mean_r/safe > 1 ~ 0, 
+                          mean_r/safe < 1 ~ 1)) %>% # determine option with higher sampled mean 
+  filter(!is.na(norm)) %>% # drop options without normative choice 
+  mutate(norm_choice = ifelse(norm == choice_pp, 1, 0)) %>% 
+  group_by(model, psi, threshold, theta, norm_choice) %>% 
+  summarise(n = n()) %>% 
+  mutate(rate = round(n/sum(n), 2)) %>% 
+  ungroup() %>%
+  filter(!(norm_choice == 0)) %>% 
   mutate(type = "CPT Posterior Predictive")
 
-##### compute proportion of correctly predicted choices on trial level
-postpred_acc <- postpred %>% 
-  select(model, psi, threshold, theta, alpha, gamma, delta, rho, problem, agent, choice_obs, choice_pp ) %>% 
+#### proportion of correctly predicted choices on trial level
+pp_acc <- postpred %>% 
+  select(model, psi, threshold, theta, alpha, gamma, delta, rho, problem, agent, choice_obs, choice_pp) %>% 
   mutate(match = if_else(choice_obs == choice_pp, 1, 0)) %>% 
   group_by(model, psi, threshold, theta, alpha, gamma, delta, rho, match) %>% 
   summarise(count = n()) %>% 
@@ -100,75 +167,106 @@ label_theta <- function(string) {
 
 #### summary comparison
 
-pp_ev_exp_max_sr <- pp_ev_exp_max %>% filter(model == "summary", threshold == "relative") 
-pp_max_summary <- obs_ev_exp_max %>%
+max_1_summary <- max_1_sr %>% 
+  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
+  facet_grid(type~theta, labeller = labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
+  geom_point(aes(shape = type), size = 3) + 
+  geom_line() + 
+  scale_x_continuous(limits = c(-.1,1.1), breaks = seq(0,1,length.out = 3)) + 
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
+  scale_shape_manual(values = c(4, 19)) + 
+  labs(x = "Switching Probability (Search Rule)" ,
+       y = "Proportion of\nMaximizing Choices" ,
+       color = "Better Average",
+       shape = "") + 
+  theme_apa(base_size = 20) + 
+  theme(strip.text.y = element_blank())
+
+pp_max_2_sr <- pp_max_2 %>% filter(model == "summary", threshold == "relative") 
+max_2_summary <- obs_max_2 %>%
   filter(model == "summary", threshold == "relative") %>% 
   ggplot(aes(psi, rate, shape = type, group = theta)) +
   scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
-  scale_y_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
-  labs(x = "Switching Probability\n(Search Rule)",
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
+  labs(x = "Switching Probability (Search Rule)",
        y = "Proportion of\n Maximizing Choices",
        shape = "") +
   facet_wrap(~theta, nrow = 1, labeller =  labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
   geom_point(size = 2) +
-  geom_point(data = pp_ev_exp_max_sr, size = 3) +
+  geom_point(data = pp_max_2_sr, size = 3) +
   scale_shape_manual(values = c(4, 19)) + 
   scale_color_scico(palette = "imola") +
   theme_minimal() + 
   theme_apa(base_size = 20)
 
-pp_acc_summary <- postpred_acc %>%  
+pp_acc_summary <- pp_acc %>%  
   filter(model == "summary", threshold == "relative") %>% 
   ggplot(aes(x = psi, y = perc)) + 
+  scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
   facet_wrap(~theta, nrow = 1, labeller = labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
-  geom_hline(yintercept = .7, linetype = "dashed") + 
-  scale_y_continuous(limits = c(.5,1), breaks = seq(.5,1,.1)) + 
   geom_point(size = 3, shape = 4) + 
-  labs(x = "Switching Probability\n(Search Rule)" , 
+  labs(x = "Switching Probability (Search Rule)" , 
        y = "Proportion of\nCorrect Predictions", 
        color = "Choice Consistency") + 
   scale_color_viridis(option = "C") + 
   theme_apa(base_size = 20)
 
-ggarrange(pp_max_summary, pp_acc_summary, nrow = 2, common.legend = T, legend = "right", labels = "AUTO")
-ggsave(file = "manuscript/figures/posterior_predictive_summary.png", width = 14, height = 7)
+ggarrange(max_1_summary, max_2_summary, pp_acc_summary, nrow = 3, common.legend = T, labels = "AUTO")
+ggsave(file = "manuscript/figures/posterior_predictive_summary.png", width = 14, height = 14)
+
 
 #### roundwise comparison
 
-#### summary comparison
+max_1_roundwise <- max_1_rr %>% 
+  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
+  facet_grid(type~theta, labeller = labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
+  geom_point(aes(shape = type), size = 3) + 
+  geom_line() + 
+  scale_x_continuous(limits = c(-.1,1.1), breaks = seq(0,1,length.out = 3)) + 
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
+  scale_shape_manual(values = c(4, 19)) + 
+  labs(x = "Switching Probability (Search Rule)" ,
+       y = "Proportion of\nMaximizing Choices" ,
+       color = "Better Average",
+       shape = "") + 
+  theme_apa(base_size = 20) + 
+  theme(strip.text.y = element_blank())
 
-pp_ev_exp_max_rr <- pp_ev_exp_max %>% filter(model == "roundwise", threshold == "relative") 
-pp_max_roundwise <- obs_ev_exp_max %>%
+pp_max_2_rr <- pp_max_2 %>% filter(model == "roundwise", threshold == "relative") 
+max_2_roundwise <- obs_max_2 %>%
   filter(model == "roundwise", threshold == "relative") %>% 
   ggplot(aes(psi, rate, shape = type, group = theta)) +
   scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
-  scale_y_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
-  labs(x = "Switching Probability\n(Search Rule)",
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
+  labs(x = "Switching Probability (Search Rule)",
        y = "Proportion of\n Maximizing Choices",
        shape = "") +
   facet_wrap(~theta, nrow = 1, labeller =  labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
   geom_point(size = 2) +
-  geom_point(data = pp_ev_exp_max_rr, size = 3) +
+  geom_point(data = pp_max_2_rr, size = 3) +
   scale_shape_manual(values = c(4, 19)) + 
   scale_color_scico(palette = "imola") +
   theme_minimal() + 
   theme_apa(base_size = 20)
 
-pp_acc_roundwise <- postpred_acc %>%  
+pp_acc_roundwise <- pp_acc %>%  
   filter(model == "roundwise", threshold == "relative") %>% 
   ggplot(aes(x = psi, y = perc)) + 
+  scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
+  scale_y_continuous(limits = c(.4, 1.1), breaks = seq(.5, 1, length.out = 3)) +
   facet_wrap(~theta, nrow = 1, labeller = labeller(theta = as_labeller(label_theta, default = label_parsed))) + 
-  geom_hline(yintercept = .7, linetype = "dashed") + 
-  scale_y_continuous(limits = c(.5,1), breaks = seq(.5,1,.1)) + 
   geom_point(size = 3, shape = 4) + 
-  labs(x = "Switching Probability\n(Search Rule)" , 
+  labs(x = "Switching Probability (Search Rule)" , 
        y = "Proportion of\nCorrect Predictions", 
        color = "Choice Consistency") + 
   scale_color_viridis(option = "C") + 
   theme_apa(base_size = 20)
 
-ggarrange(pp_max_roundwise, pp_acc_roundwise, nrow = 2, common.legend = T, legend = "right", labels = "AUTO")
-ggsave(file = "manuscript/figures/posterior_predictive_roundwise.png", width = 14, height = 7)
+ggarrange(max_1_roundwise, max_2_roundwise, pp_acc_roundwise, nrow = 3, common.legend = T, labels = "AUTO")
+ggsave(file = "manuscript/figures/posterior_predictive_roundwise.png", width = 14, height = 14)
+
+# generated vs. predicted choice proportions ------------------------------
 
 # the lower psi and the higher the elevation, the more risky choices are predicted
 # i.e., with low switching probabilities, small but systematic CPT bias towards the risky option 
@@ -227,207 +325,10 @@ postpred %>%
   scale_y_continuous(limits = c(.3,.8))
 
 
-# test --------------------------------------------------------------------
-
-# for low psi, bias towards safe option is larger
-
-test <- choices %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-p1 <- test %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() +
-  scale_y_continuous(limits = c(.5,1))
-
-
-test2 <- postpred %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(choice_pp_2 = ifelse(choice_pp == 0, "r", "s"), 
-         norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice_pp_2 == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-
-p2 <- test2 %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() + 
-  scale_y_continuous(limits = c(.5,1))
-
-
-p1 + p2 + plot_layout(nrow = 2)
-
-
-test3 <- choices %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-p3 <- test3 %>% 
-  ggplot(aes(x=psi, y=prop)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line()
-
-test4 <- postpred %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(choice_pp_2 = ifelse(choice_pp == 0, "r", "s"), 
-         norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice_pp_2 == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-p4 <- test4 %>% 
-  ggplot(aes(x=psi, y=prop)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line()
-
-p3 + p4 + plot_layout(nrow = 2)
-
-
-
-
-test3 <- choices %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1) %>% 
-  mutate(norm = "Free")
-
-test5 <- choices %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-p5 <- test5 %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() + 
-  geom_line(data = test3) + 
-  geom_point(data = test3) + 
-  labs(title = "Sampling Strategy Maximization") + 
-  theme_minimal()
-
-p5
-
-
-test4 <- postpred %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(choice_pp_2 = ifelse(choice_pp == 0, "r", "s"), 
-         norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice_pp_2 == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1) %>% 
-  mutate(norm = "Free")
-
-
-test6 <- postpred %>% 
-  filter(model == "summary", threshold == "relative") %>% 
-  mutate(choice_pp_2 = ifelse(choice_pp == 0, "r", "s"), 
-         norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice_pp_2 == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-
-p6 <- test6 %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() + 
-  geom_line(data = test4) + 
-  geom_point(data = test4) + 
-  labs(title = "Posterior Predictive Maximization") +
-  theme_minimal()
-
-p5 + p6 + plot_layout(nrow = 2)
-
-
-
-cpt %>% filter(parameter == "deviance") %>% 
-  group_by(model, psi, threshold, theta) %>% 
-  summarize(mdev = mean) %>% 
-  View()
 
 
 
 
 
 
-test <- choices %>% 
-  filter(model == "roundwise", threshold == "relative") %>% 
-  mutate(norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
 
-p1 <- test %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() + 
-  scale_y_continuous(limits = c(.5,1))
-
-
-test2 <- postpred %>% 
-  filter(model == "roundwise", threshold == "relative") %>% 
-  mutate(choice_pp_2 = ifelse(choice_pp == 0, "r", "s"), 
-         norm = case_when(mean_r/safe > 1 ~ "r", mean_r/safe < 1 ~ "s"), 
-         norm_choice = ifelse(choice_pp_2 == norm, 1, 0)) %>% 
-  filter(!is.na(norm)) %>% 
-  group_by(psi, theta, norm, norm_choice) %>% 
-  summarise(n_norm = n()) %>% 
-  mutate(prop = round(n_norm/sum(n_norm),2)) %>% 
-  filter(norm_choice == 1)
-
-
-p2 <- test2 %>% 
-  ggplot(aes(x=psi, y=prop, group = norm, color = norm)) + 
-  facet_wrap(~theta, nrow = 1) + 
-  geom_point() + 
-  geom_line() + 
-  scale_y_continuous(limits = c(.5,1))
-
-p1 + p2 + plot_layout(nrow = 2)

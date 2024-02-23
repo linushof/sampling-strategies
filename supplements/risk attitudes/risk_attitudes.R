2fbe6bdb136e99cb644377368d8e4c9af6170466
linushof <linus.hof@tum.de>
2024-02-23 13:00:31 +0100

    add analysis/pattern of overall risk aversion without EV confound

--- supplements/risk attitudes/risk_attitudes.R
+++ supplements/risk attitudes/risk_attitudes.R
@@ -12,6 +12,7 @@ risk_rates <- choices %>%
   ungroup()
 
 
+# granular pattern 
 
 risk_rates <- choices %>% 
   filter(!c(n_s == 0 | n_r == 0)) %>% # remove choices where an option was not attended 
@@ -33,6 +34,7 @@ risk_rates %>%
                                             psi = as_labeller(label_psi, default = label_parsed))) +
   geom_bar(stat="identity", position = "stack") + 
   geom_point() + 
+  scale_y_continuous(limits = c(0, .3), breaks = seq(0,.3,.05)) +
   labs(x="Risk Attitude",
        y="Proportion", 
        fill="Risk Attitude",
@@ -48,6 +50,7 @@ risk_rates %>%
                                             psi = as_labeller(label_psi, default = label_parsed))) +
   geom_bar(stat="identity", position = "stack") + 
   geom_point() + 
+  scale_y_continuous(limits = c(0, .3), breaks = seq(0,.3,.05)) +
   labs(x="Risk Attitude",
        y="Proportion", 
        fill="Risk Attitude",
@@ -55,4 +58,67 @@ risk_rates %>%
        subtitle = "Roundwise Comparison Rule") + 
   theme_apa()
 ggsave("supplements/risk attitudes/risk_attitudes_roundwise.png", width=16,height=9)
+
+
+# Overall pattern
+
+## expected number of safe choices under risk neutrality
+problems <- choices <- read_rds("data/choice_problems.rds") 
+neutral_rates <- problems %>% 
+  mutate(better_safe = case_when(safe > r_ev ~ 1, 
+                                 safe < r_ev ~ 0, 
+                                 safe == r_ev ~ NA)) %>% 
+  group_by(better_safe) %>% 
+  summarise(count = n()) %>% 
+  mutate(prop = count/sum(count))
+safe_prop <- round(neutral_rates[[2,3]], 3)
+## Safe proportion = .533
+
+rates <- choices %>% 
+  filter(!c(n_s == 0 | n_r == 0)) %>% # remove choices where an option was not attended 
+  mutate(r_averse = ifelse(choice == "s", 1, 0)) %>% # risk averse choice
+  group_by(model, psi, threshold, theta, r_averse) %>% 
+  summarise(n = n()) %>% 
+  mutate(rate = round(n/sum(n), 2)) %>% 
+  ungroup() %>% 
+  filter(!(r_averse == 0)) %>% 
+  mutate(rate_EV_control = (rate -safe_prop)*100)
+
+
+r_averse_summary <- rates %>%  
+  filter(model == "summary" & threshold == "relative") %>% 
+  #filter(psi > .9 | psi == .5 | psi == (1-.9)) %>% 
+  ggplot(aes(psi, rate_EV_control, group = theta, color = theta)) +
+  scale_color_scico(palette = "imola", alpha = .7) +
+  scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
+  scale_y_continuous(limits = c(-10, 10), breaks = seq(-10, 10, 1)) +
+  labs(title = "Summary Comparison", 
+       x = "Switching Probability\n(Search Rule)",
+       y =  "% Safe Choices \n Observed - Expected under EV max.",
+       color = "Threshold\n(Stopping Rule)") +
+  geom_line(linewidth = 1) + 
+  geom_point(size = 3) +
+  geom_hline(yintercept = 0) +
+  theme_minimal(base_size = 20)
+
+r_averse_roundwise <- rates %>%
+  filter(model == "roundwise" & threshold == "relative") %>% 
+  #filter(psi > .9 | psi == .5 | psi == (1-.9)) %>% 
+  ggplot(aes(psi, rate_EV_control, group = theta, color = as.factor(theta))) +
+  scale_color_scico_d(palette = "imola", alpha = .7) + 
+  scale_x_continuous(limits = c(-.1, 1.1), breaks = seq(0, 1, length.out = 3)) +
+  scale_y_continuous(limits = c(-10, 10), breaks = seq(-10, 10, 1)) +
+  labs(title = "Roundwise Comparison", 
+       x = "Switching Probability\n(Search Rule)",
+       y =  "% Safe Choices \n Observed - Expected under EV max.",
+       color = "Threshold\n(Stopping Rule)") +
+  geom_line(linewidth = 1) + 
+  geom_point(size = 3) +
+  geom_hline(yintercept = 0) +
+  theme_minimal(base_size = 20)
+
+r_averse_summary + r_averse_roundwise + plot_annotation(tag_levels = "A")
+ggsave(file = "supplements/risk attitudes/rates_safe_control.png", width = 14, height = 6)
+
+
 

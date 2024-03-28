b63705220a119a4186c7b7f85dbf702eef839b44
linushof <linus.hof@tum.de>
2024-03-28 11:29:23 +0100

    completion of summary strategy: recoding names of problems features, store additional sampling and accumulation data, use base R where possible

--- code/model_implementation_summary.R
+++ code/model_implementation_summary.R
@@ -1,14 +1,13 @@
 # load pkgs
 pacman::p_load(tidyverse, digest, crayon)
-source("code/helper_functions/fun_compute_cumulative_stats.R") # call functions for computing cumulative stats
 
 # test set
-problems <- read_rds("data/choice_problems.rds")
-n_agents <- 200 # number of synthetic agents
+problems <- as.data.frame(readRDS("data/choice_problems_balanced.rds"))
+n_agents <- 10 # number of synthetic agents
 
 # simulation parameters
 param <- expand.grid(psi = seq(.1, 1, .1) , # switching probability
-                     theta = seq(15, 75, 15)) # thresholds
+                     theta = seq(10, 50, 10)) # thresholds
 
 # simulation: for each parameter combination (rows of param), all choice problems (rows of choice_problems) are solved by all agents
 
@@ -19,23 +18,37 @@ for (set in seq_len(nrow(param))) { # loop over parameter combinations
   psi <- param[[set,"psi"]]
   theta <- param[[set, "theta"]]
   
-  problem_list <- vector("list", nrow(choice_problems))
+  problem_list <- vector("list", nrow(problems))
   
-  for (problem in seq_len(nrow(choice_problems))) { # loop over choice problems
+  for (problem in seq_len(nrow(problems))) { # loop over choice problems
     
     # retrieve problem features here
+    p_r_1 <- problems[[problem, "p_r_1"]]
+    p_r_2 <- problems[[problem, "p_r_2"]]
+    r_1 <- problems[[problem, "r_1"]]
+    r_2 <- problems[[problem, "r_2"]]
+    safe <- problems[[problem, "safe"]]
     
     agents_list <- vector("list", n_agents)
+    
     for (agent in seq_along(1:n_agents)){ # loop over agents
 
       ## parameters to initiate trials with
       
       attended <- NULL
+      sample_n <- 0
+      samples_n_r <- 0
+      samples_n_s <- 0
       samples_r <- NULL
       samples_s <- NULL
-      DV_risky <- NULL
-      DV_safe <- NULL
+      DV_risky <- 0
+      DV_safe <- 0
+      
+      sample_trace <- NULL
+      DV_risky_trace <- NULL
+      DV_safe_trace <- NULL
       DV_trace <- NULL
+      choice_trace <- NULL
       
       boundary_reached <- FALSE
       
@@ -47,23 +60,33 @@ for (set in seq_len(nrow(param))) { # loop over parameter combinations
       while(boundary_reached == FALSE) {
         
         attended <- c(attended, attend)
+        sample_n <- sample_n + 1
+        sample_trace <- c(sample_trace, sample_n)
         
         ### draw sample and update evidence for respective option
 
         if(attend == "r") {
           
-          sampled_outcome <- sample(x = c(r_low, r_high), size = 1, prob = c(p_r_low, p_r_high))
+          samples_n_r <- samples_n_r + 1
+          
+          sampled_outcome <- sample(x = c(r_1, r_2), size = 1, prob = c(p_r_1, p_r_2))
           samples_r <- c(samples_r, sampled_outcome)
           samples_s <- c(samples_s, NA)
           DV_risky <- DV_risky + sampled_outcome
+          DV_risky_trace <- c(DV_risky_trace, DV_risky)
+          DV_safe_trace <- c(DV_safe_trace, DV_safe)
           
           p_attend_r <- 1-psi
           
           } else {
             
+            samples_n_s <- samples_n_s + 1
+            
             samples_s <- c(samples_s, safe)
             samples_r <- c(samples_r, NA)
             DV_safe <- DV_safe + safe
+            DV_safe_trace <- c(DV_safe_trace, DV_safe)
+            DV_risky_trace <- c(DV_risky_trace, DV_risky)
             
             p_attend_r <- psi
             
@@ -71,29 +94,36 @@ for (set in seq_len(nrow(param))) { # loop over parameter combinations
         
         ### check if accumulated evidence reached threshold
         
+        if(samples_n_s > 0 & samples_n_r > 0){
         DV <- DV_risky - DV_safe # returns NA if not at least one sample from each option has been drawn
         DV_trace <- c(DV_trace, DV)
+        } else {
+          DV <- 0
+          DV_trace <- c(DV_trace, DV)
+        }
+        
         choice <- ifelse(DV >= theta, "r", ifelse(DV <= -1*theta, "s", NA))
+        choice_trace <- c(choice_trace, choice)
 
         ### if threshold isn't reached, draw new sample according to psi
 
         if(is.na(choice)) {
           attend <- sample(c("r", "s"), size=1, prob=c(p_attend_r, 1-p_attend_r))
-        } else {
-          boundary_reached <- TRUE
+          } else {
+            boundary_reached <- TRUE
         }
         
       } # close loop choice trial
-      agents_list[[agent]] <- expand.grid(agent, fd)
+      agents_list[[agent]] <- data.frame(agent, sample_trace, attended, samples_r, samples_s, DV_risky_trace, DV_safe_trace,DV_trace, choice_trace)
     } # close loop agents
-    all_agents <- agents_list %>% bind_rows()
-    problem_list[[problem]] <- expand.grid(problem, all_agents)
-    print(paste("\u2713 Parameter Set No. ", set, ", Problem No. ", problem, " finished!"))
+    all_agents <- bind_rows(agents_list)
+    problem_list[[problem]] <- as.data.frame(expand_grid(problems[problem, ], all_agents))
   } # close loop choice problems
-  all_problems <- problem_list %>% bind_rows()
-  param_list[[set]] <- expand.grid(param[set, ], all_problems)
+  all_problems <- bind_rows(problem_list) 
+  param_list[[set]] <- as.data.frame(expand_grid(param[set, ], all_problems))
+  print(paste("\u2713 Parameter Set No. ", set, " finished!"))
 } # close loop parameters
-simulation_summary <- param_list %>% bind_rows()
+simulation_summary <- bind_rows(param_list)
 
 # save data  
 simulation_summary <- read_rds("data/merged/simulation_summary_merged.rds.bz2")

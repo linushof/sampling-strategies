d39d8ef7ce21d88202c2d42ec103854d83f3b6e8
Linus Hof <linus.hof@tum.de>
2025-06-19 13:55:50 +0200

    1) improved generation/filtering of risky risky problems:
    use mutate and group_by to get rid of redundant code;
    consider options' risk before filtering;
    consider rare events for both options separately
    
    2) tidy script with simulation calls

--- code/select_gambles.R
+++ code/select_gambles.R
@@ -1,37 +1,70 @@
 # preparation -------------------------------------------------------------
 
 # packages
-library(pacman)
-p_load(tidyverse, magrittr)
+library(tidyverse)
 
-# functions
-source("code/helper_functions/fun_gambles_safe.R") # call function for generating choice problems
+# import functions for generating choice problems
 source("code/helper_functions/fun_gambles_risky.R")
+# source("code/helper_functions/fun_gambles_safe.R")
 
-# initial problem settings ------------------------------------------------
+# problem/filter settings ------------------------------------------------
 
-N <- 1e7
+N <- 1e6 #1e7
 lower <- 0 # lowest possible outcome 
 upper <- 100 # highest possible outcome
 pmin <- .1 # lowest possible probability 
 pmin_com <- .4 # lowest probability for non-rare events
 pmax_rare <- .2 # highest probability for rare events
-N_trials <- 60 # number of trials per agent
+N_trials <- 200 # number of trials per agent
 diffs <- seq(.05,.25,.1) # magnitude of EV differences
 
-# generate problems -------------------------------------------------------
+# generate problem pool -------------------------------------------------------
 
-## safe-risky --------------------------------------------------------------
+set.seed(123)
+RR <- RR_gambles(N, lower, upper, pmin)
 
-set.seed(4564)
-SR <- SR_gambles(N, lower, upper, pmin)
+# prepare problems -----------------------------------------------------
 
-SR <- SR %>% 
-  mutate(
-    ev_o1 = o1_1*o1_p1 + o1_2*o1_p2 ,
-    ev_o2 = o2_1*o2_p1 + o2_2*o2_p2 , 
-    ev_diff = ev_o1-ev_o2 , 
-    ev_diff_sc = abs(ev_diff)/upper , 
+RR <- RR |>
+  
+  # we start by determining which option is more risky (higher coefficient of variation)
+  # we label the more (less) risky option o1 (o2) - similarly to safe-risky problems, where o1 (o2) is the risky (safe) option
+  
+  mutate( 
+    cvar_o1 = sqrt( round( (o1_1^2) * o1_p1 + (o1_2^2) * o1_p2, 4 ) - round((o1_1 * o1_p1 + o1_2 * o1_p2)^2, 4 ))  ,
+    cvar_o2 = sqrt( round( (o2_1^2) * o2_p1 + (o2_2^2) * o2_p2, 4 ) - round((o2_1 * o2_p1 + o2_2 * o2_p2)^2, 4 ))  ,
+    higher_risk = if_else(cvar_o1 > cvar_o2, "o1", "o2") , 
+    o1_1_new = if_else(cvar_o1 > cvar_o2, o1_1, o2_1) ,
+    o1_p1_new = if_else(cvar_o1 > cvar_o2, o1_p1, o2_p1) ,
+    o1_2_new = if_else(cvar_o1 > cvar_o2, o1_2, o2_2) ,
+    o1_p2_new = if_else(cvar_o1 > cvar_o2, o1_p2, o2_p2) ,
+    o2_1_new = if_else(cvar_o1 > cvar_o2, o2_1, o1_1) ,
+    o2_p1_new = if_else(cvar_o1 > cvar_o2, o2_p1, o1_p1) ,
+    o2_2_new = if_else(cvar_o1 > cvar_o2, o2_2, o1_2) ,
+    o2_p2_new = if_else(cvar_o1 > cvar_o2, o2_p2, o1_p2) 
+  ) |>
+  select(o1_1_new:o2_p2_new) |> 
+  rename(o1_1 = o1_1_new ,
+         o1_p1 = o1_p1_new ,
+         o1_2 = o1_2_new ,
+         o1_p2 = o1_p2_new ,
+         o2_1 = o2_1_new ,
+         o2_p1 = o2_p1_new ,
+         o2_2 = o2_2_new ,
+         o2_p2 = o2_p2_new) |> 
+  
+  # now we calculate some important properties for both options 
+  
+  mutate( 
+    o1_ev = round( o1_1*o1_p1 + o1_2*o1_p2 , 2) ,
+    o2_ev = round( o2_1*o2_p1 + o2_2*o2_p2 , 2) ,
+    ev_diff = o1_ev-o2_ev , 
+    ev_diff_sc = round(abs(ev_diff)/upper , 2) , 
+    var_o1 = o1_p1 * o1_p2 , 
+    var_o2 = o2_p1 * o2_p2 ,
+    cvar_o1 = sqrt( round( (o1_1^2) * o1_p1 + (o1_2^2) * o1_p2, 4 ) - round((o1_1 * o1_p1 + o1_2 * o1_p2)^2, 4 ))  ,
+    cvar_o2 = sqrt( round( (o2_1^2) * o2_p1 + (o2_2^2) * o2_p2, 4 ) - round((o2_1 * o2_p1 + o2_2 * o2_p2)^2, 4 ))  ,
+    higher_risk = if_else(cvar_o1 > cvar_o2, "o1", "o2") , 
     c11 = if_else(o1_1 > o2_1, 1, 0) , 
     c12 = if_else(o1_1 > o2_2, 1, 0) , 
     c21 = if_else(o1_2 > o2_1, 1, 0) ,
@@ -46,249 +79,61 @@ SR <- SR %>%
     o1_plarge = 1-o1_psmall , 
     o2_psmall = if_else(o2_1 < o2_2, o2_p1, o2_p2) , 
     o2_plarge = 1-o2_psmall
-    ) %>%
+  ) |> 
+  
   select(-c(c11:cp22))
 
-## risky-risky -------------------------------------------------------------------------
+# apply filters -----------------------------------------------------------
+# We stratify the problem pool by applying a sequence of filters. 
+# To create the problem set for simulation, we randomly sample from each of the strata/subgroups.
+# Stratified sampling prevents a spurious/unbalanced distribution of problem features
+# and ensures an interpretable baseline behavior in the aggregate (across all problems).
 
-set.seed(1234)
-RR <- RR_gambles(N, lower, upper, pmin)
+RR <- RR |> 
+  mutate(better_ev = if_else(o1_ev > o2_ev, "o1", "o2") , # Filter 1: option with better EV
+         better_rwp = if_else(o1_rwp > o2_rwp, "o1", "o2") , # Filter 2: option wither roundwise winning probability (rwp)
+         o1_rare = case_when(
+           o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com) ~ "none" ,
+           o1_psmall <= pmax_rare ~ "unattractive" ,
+           o1_plarge <= pmax_rare  ~ "attractive"
+         ) ,
+         o2_rare = case_when(
+           o2_p1 >= pmin_com & o2_p1 <= (1-pmin_com) ~ "none" ,
+           o2_psmall <= pmax_rare ~ "unattractive" ,
+           o2_plarge <= pmax_rare  ~ "attractive"
+         )
+  ) 
 
-RR %<>% mutate(
-  ev_o1 = o1_1*o1_p1 + o1_2*o1_p2 ,
-  ev_o2 = o2_1*o2_p1 + o2_2*o2_p2 , 
-  ev_diff = ev_o1-ev_o2 , 
-  ev_diff_sc = abs(ev_diff)/upper , 
-  c11 = if_else(o1_1 > o2_1, 1, 0) , 
-  c12 = if_else(o1_1 > o2_2, 1, 0) , 
-  c21 = if_else(o1_2 > o2_1, 1, 0) ,
-  c22 = if_else(o1_2 > o2_2, 1, 0) ,
-  cp11 = o1_p1 * o2_p1 ,
-  cp12 = o1_p1 * o2_p2 , 
-  cp21 = o1_p2 * o2_p1 ,
-  cp22 = o1_p2 * o2_p2 ,
-  o1_rwp = c11*cp11 + c12*cp12 + c21*cp21 + c22*cp22 , 
-  o2_rwp = 1-o1_rwp ,
-  o1_psmall = if_else(o1_1 < o1_2, o1_p1, o1_p2) , 
-  o1_plarge = 1-o1_psmall , 
-  o2_psmall = if_else(o2_1 < o2_2, o2_p1, o2_p2) , 
-  o2_plarge = 1-o2_psmall
-  ) %>% 
-  select(-c(c11:cp22))
+groups <-  
+  RR |>
+  drop_na(o1_rare, o2_rare) |> 
+  group_by (better_ev, better_rwp, o1_rare, o2_rare, .drop = FALSE) |>
+  summarise(group = cur_group_id() ) 
 
+# sample simulation set ---------------------------------------------------------
 
-# filter problems -----------------------------------------------------------
+problems <- tibble()
+for (i in seq_len(nrow(groups)) ){ 
+  
+  dat_temp <- RR |> 
+    # filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])  |> to filter for particular EV diffs
+    filter(better_ev==groups[[i, "better_ev"]] & 
+             better_rwp==groups[[i, "better_rwp"]] & 
+             o1_rare==groups[[i, "o1_rare"]] & 
+             o2_rare==groups[[i, "o2_rare"]]
+    )
+  
+  smp_tmp <- dat_temp[ sample(1:nrow(dat_temp), 10, replace=F) , ] 
+  
+  problems <- bind_rows(problems, smp_tmp)
+  
+}
 
-
-## safe-risky --------------------------------------------------------------
-
-
-### 1) EV direction -------------------------------------------------------------------------
-
-SR_o1 <- SR %>% filter(ev_diff > 0)
-SR_o2 <- SR %>% filter(ev_diff < 0)
-
-### 2) ordinal comparisons -------------------------------------------
-
-SR_o1_o1 <- SR_o1 %>% filter(o1_rwp > .5)
-SR_o1_o2 <- SR_o1 %>% filter(o2_rwp > .5)
-SR_o2_o1 <- SR_o2 %>% filter(o1_rwp > .5)
-SR_o2_o2 <- SR_o2 %>% filter(o2_rwp > .5)
-
-
-### 3) rare events -------------------------------------------------------------
-
-# no rare event
-SR_o1_o1_N <- SR_o1_o1 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com))
-SR_o1_o2_N <- SR_o1_o2 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com))
-SR_o2_o1_N <- SR_o2_o1 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com))
-SR_o2_o2_N <- SR_o2_o2 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com))
-
-# unattractive
-SR_o1_o1_U <- SR_o1_o1 %>% filter(o1_psmall <= pmax_rare) 
-SR_o2_o1_U <- SR_o2_o1 %>% filter(o1_psmall <= pmax_rare) 
-
-# attractive
-SR_o1_o2_A <- SR_o1_o2 %>% filter(o1_plarge <= pmax_rare) 
-SR_o2_o2_A <- SR_o2_o2 %>% filter(o1_plarge <= pmax_rare)
-
-### 4) EV magnitude ------------------------------------------------------------
-
-# small (<= 20% outcome range)
-SR_o1_o1_N_S <- SR_o1_o1_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o1_o2_N_S <- SR_o1_o2_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o2_o1_N_S <- SR_o2_o1_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o2_o2_N_S <- SR_o2_o2_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o1_o1_U_S <- SR_o1_o1_U %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o2_o1_U_S <- SR_o2_o1_U %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-SR_o1_o2_A_S <- SR_o1_o2_A %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])   
-SR_o2_o2_A_S <- SR_o2_o2_A %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-
-# large (> 20% & < 40% outcome range)
-SR_o1_o1_N_L <- SR_o1_o1_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o1_o2_N_L <- SR_o1_o2_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o2_o1_N_L <- SR_o2_o1_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o2_o2_N_L <- SR_o2_o2_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o1_o1_U_L <- SR_o1_o1_U %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o2_o1_U_L <- SR_o2_o1_U %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-SR_o1_o2_A_L <- SR_o1_o2_A %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])   
-SR_o2_o2_A_L <- SR_o2_o2_A %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-
-
-## risky-risky -------------------------------------------------------------
-
-
-### 1) EV direction -------------------------------------------------------------------------
-
-RR_o1 <- RR %>% filter(ev_diff > 0)
-RR_o2 <- RR %>% filter(ev_diff < 0)
-
-### 2) ordinal comparisons -------------------------------------------
-
-RR_o1_o1 <- RR_o1 %>% filter(o1_rwp > .5)
-RR_o1_o2 <- RR_o1 %>% filter(o2_rwp > .5)
-RR_o2_o1 <- RR_o2 %>% filter(o1_rwp > .5)
-RR_o2_o2 <- RR_o2 %>% filter(o2_rwp > .5)
-
-### 3) rare events -------------------------------------------------------------
-
-# no rare event
-RR_o1_o1_N <- RR_o1_o1 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com) & o2_p1 >= pmin_com & o2_p1 <= (1-pmin_com) )
-RR_o1_o2_N <- RR_o1_o2 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com) & o2_p1 >= pmin_com & o2_p1 <= (1-pmin_com) )
-RR_o2_o1_N <- RR_o2_o1 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com) & o2_p1 >= pmin_com & o2_p1 <= (1-pmin_com) )
-RR_o2_o2_N <- RR_o2_o2 %>% filter(o1_p1 >= pmin_com & o1_p1 <= (1-pmin_com) & o2_p1 >= pmin_com & o2_p1 <= (1-pmin_com) )
-
-# unattractive
-RR_o1_o1_U <- SR_o1_o1 %>% filter( (o1_psmall <= pmax_rare | o2_psmall <= pmax_rare), !(o1_psmall <= pmax_rare & o2_psmall <= pmax_rare) ) 
-RR_o2_o1_U <- SR_o2_o1 %>% filter( (o1_psmall <= pmax_rare | o2_psmall <= pmax_rare), !(o1_psmall <= pmax_rare & o2_psmall <= pmax_rare) ) 
-
-# attractive
-RR_o1_o2_A <- SR_o1_o2 %>% filter(  (o1_plarge <= pmax_rare | o2_plarge <= pmax_rare), !(o1_plarge <= pmax_rare & o2_plarge <= pmax_rare)  ) 
-RR_o2_o2_A <- SR_o2_o2 %>% filter(  (o1_plarge <= pmax_rare | o2_plarge <= pmax_rare), !(o1_plarge <= pmax_rare & o2_plarge <= pmax_rare)  )
-
-### 4) EV magnitude ------------------------------------------------------------
-
-# small (<= 20% outcome range)
-RR_o1_o1_N_S <- SR_o1_o1_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o1_o2_N_S <- SR_o1_o2_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o2_o1_N_S <- SR_o2_o1_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o2_o2_N_S <- SR_o2_o2_N %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o1_o1_U_S <- SR_o1_o1_U %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o2_o1_U_S <- SR_o2_o1_U %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-RR_o1_o2_A_S <- SR_o1_o2_A %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])   
-RR_o2_o2_A_S <- SR_o2_o2_A %>% filter(ev_diff_sc > diffs[1] & ev_diff_sc <= diffs[2])
-
-# large (> 20% & < 40% outcome range)
-RR_o1_o1_N_L <- RR_o1_o1_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o1_o2_N_L <- RR_o1_o2_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o2_o1_N_L <- RR_o2_o1_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o2_o2_N_L <- RR_o2_o2_N %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o1_o1_U_L <- RR_o1_o1_U %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o2_o1_U_L <- RR_o2_o1_U %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-RR_o1_o2_A_L <- RR_o1_o2_A %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])   
-RR_o2_o2_A_L <- RR_o2_o2_A %>% filter(ev_diff_sc > diffs[2] & ev_diff_sc <= diffs[3])
-
-# sample problems ---------------------------------------------------------
-
-## safe-risky --------------------------------------------------------------
-
-# small
-SR_o1_o1_N_S_S <- SR_o1_o1_N_S[ sample(1:nrow(SR_o1_o1_N_S), N_trials/12, replace=F) , ] 
-SR_o1_o2_N_S_S <- SR_o1_o2_N_S[ sample(1:nrow(SR_o1_o2_N_S), N_trials/12, replace=F) , ]  
-SR_o2_o1_N_S_S <- SR_o2_o1_N_S[ sample(1:nrow(SR_o2_o1_N_S), N_trials/12, replace=F) , ] 
-SR_o2_o2_N_S_S <- SR_o2_o2_N_S[ sample(1:nrow(SR_o2_o2_N_S), N_trials/12, replace=F) , ] 
-SR_o1_o1_U_S_S <- SR_o1_o1_U_S[ sample(1:nrow(SR_o1_o1_U_S), N_trials/6, replace=F) , ] 
-SR_o2_o1_U_S_S <- SR_o2_o1_U_S[ sample(1:nrow(SR_o2_o1_U_S), N_trials/6, replace=F) , ] 
-SR_o1_o2_A_S_S <- SR_o1_o2_A_S[ sample(1:nrow(SR_o1_o2_A_S), N_trials/6, replace=F) , ]    
-SR_o2_o2_A_S_S <- SR_o2_o2_A_S[ sample(1:nrow(SR_o2_o2_A_S), N_trials/6, replace=F) , ]
-
-SR_S <- bind_rows(
-  SR_o1_o1_N_S_S ,
-  SR_o1_o2_N_S_S ,  
-  SR_o2_o1_N_S_S , 
-  SR_o2_o2_N_S_S , 
-  SR_o1_o1_U_S_S , 
-  SR_o2_o1_U_S_S , 
-  SR_o1_o2_A_S_S ,    
-  SR_o2_o2_A_S_S
-  ) %>% 
-  mutate(id = row_number())
-
-
-# large
-SR_o1_o1_N_L_S <- SR_o1_o1_N_L[ sample(1:nrow(SR_o1_o1_N_L), N_trials/12, replace=F) , ] 
-SR_o1_o2_N_L_S <- SR_o1_o2_N_L[ sample(1:nrow(SR_o1_o2_N_L), N_trials/12, replace=F) , ]  
-SR_o2_o1_N_L_S <- SR_o2_o1_N_L[ sample(1:nrow(SR_o2_o1_N_L), N_trials/12, replace=F) , ] 
-SR_o2_o2_N_L_S <- SR_o2_o2_N_L[ sample(1:nrow(SR_o2_o2_N_L), N_trials/12, replace=F) , ] 
-SR_o1_o1_U_L_S <- SR_o1_o1_U_L[ sample(1:nrow(SR_o1_o1_U_L), N_trials/6, replace=F) , ] 
-SR_o2_o1_U_L_S <- SR_o2_o1_U_L[ sample(1:nrow(SR_o2_o1_U_L), N_trials/6, replace=F) , ] 
-SR_o1_o2_A_L_S <- SR_o1_o2_A_L[ sample(1:nrow(SR_o1_o2_A_L), N_trials/6, replace=F) , ]    
-SR_o2_o2_A_L_S <- SR_o2_o2_A_L[ sample(1:nrow(SR_o2_o2_A_L), N_trials/6, replace=F) , ]
-
-SR_L <- bind_rows(
-  SR_o1_o1_N_L_S ,
-  SR_o1_o2_N_L_S ,  
-  SR_o2_o1_N_L_S , 
-  SR_o2_o2_N_L_S , 
-  SR_o1_o1_U_L_S , 
-  SR_o2_o1_U_L_S , 
-  SR_o1_o2_A_L_S ,    
-  SR_o2_o2_A_L_S
-) %>% 
-  mutate(id = row_number())
-
-
-## risky-risky -------------------------------------------------------------
-
-# small
-RR_o1_o1_N_S_S <- RR_o1_o1_N_S[ sample(1:nrow(RR_o1_o1_N_S), N_trials/12, replace=F) , ] 
-RR_o1_o2_N_S_S <- RR_o1_o2_N_S[ sample(1:nrow(RR_o1_o2_N_S), N_trials/12, replace=F) , ]  
-RR_o2_o1_N_S_S <- RR_o2_o1_N_S[ sample(1:nrow(RR_o2_o1_N_S), N_trials/12, replace=F) , ] 
-RR_o2_o2_N_S_S <- RR_o2_o2_N_S[ sample(1:nrow(RR_o2_o2_N_S), N_trials/12, replace=F) , ] 
-RR_o1_o1_U_S_S <- RR_o1_o1_U_S[ sample(1:nrow(RR_o1_o1_U_S), N_trials/6, replace=F) , ] 
-RR_o2_o1_U_S_S <- RR_o2_o1_U_S[ sample(1:nrow(RR_o2_o1_U_S), N_trials/6, replace=F) , ] 
-RR_o1_o2_A_S_S <- RR_o1_o2_A_S[ sample(1:nrow(RR_o1_o2_A_S), N_trials/6, replace=F) , ]    
-RR_o2_o2_A_S_S <- RR_o2_o2_A_S[ sample(1:nrow(RR_o2_o2_A_S), N_trials/6, replace=F) , ]
-
-RR_S <- bind_rows(
-  RR_o1_o1_N_S_S ,
-  RR_o1_o2_N_S_S ,  
-  RR_o2_o1_N_S_S , 
-  RR_o2_o2_N_S_S , 
-  RR_o1_o1_U_S_S , 
-  RR_o2_o1_U_S_S , 
-  RR_o1_o2_A_S_S ,    
-  RR_o2_o2_A_S_S
-) %>% 
-  mutate(id = row_number())
-
-
-# large
-RR_o1_o1_N_L_S <- RR_o1_o1_N_L[ sample(1:nrow(RR_o1_o1_N_L), N_trials/12, replace=F) , ] 
-RR_o1_o2_N_L_S <- RR_o1_o2_N_L[ sample(1:nrow(RR_o1_o2_N_L), N_trials/12, replace=F) , ]  
-RR_o2_o1_N_L_S <- RR_o2_o1_N_L[ sample(1:nrow(RR_o2_o1_N_L), N_trials/12, replace=F) , ] 
-RR_o2_o2_N_L_S <- RR_o2_o2_N_L[ sample(1:nrow(RR_o2_o2_N_L), N_trials/12, replace=F) , ] 
-RR_o1_o1_U_L_S <- RR_o1_o1_U_L[ sample(1:nrow(RR_o1_o1_U_L), N_trials/6, replace=F) , ] 
-RR_o2_o1_U_L_S <- RR_o2_o1_U_L[ sample(1:nrow(RR_o2_o1_U_L), N_trials/6, replace=F) , ] 
-RR_o1_o2_A_L_S <- RR_o1_o2_A_L[ sample(1:nrow(RR_o1_o2_A_L), N_trials/6, replace=F) , ]    
-RR_o2_o2_A_L_S <- RR_o2_o2_A_L[ sample(1:nrow(RR_o2_o2_A_L), N_trials/6, replace=F) , ]
-
-RR_L <- bind_rows(
-  RR_o1_o1_N_L_S ,
-  RR_o1_o2_N_L_S ,  
-  RR_o2_o1_N_L_S , 
-  RR_o2_o2_N_L_S , 
-  RR_o1_o1_U_L_S , 
-  RR_o2_o1_U_L_S , 
-  RR_o1_o2_A_L_S ,    
-  RR_o2_o2_A_L_S
-) %>% 
-  mutate(id = row_number())
-
+problems |> 
+  group_by(better_ev) |> 
+  summarise(ev_mean=mean(ev_diff_sc) , 
+            rwp_mean=mean(o1_rwp))
 
 # store problems ----------------------------------------------------------
 
-write_rds(SR_S, 'revision/data/SR_S_problems.rds')
-write_rds(SR_L, 'revision/data/SR_L_problems.rds')
-write_rds(RR_S, 'revision/data/RR_S_problems.rds')
-write_rds(RR_L, 'revision/data/RR_L_problems.rds')
+write_rds(problems, 'data/problems/RR_large_rand.rds')
--- code/simulations.R
+++ code/simulations.R
@@ -1,9 +1,11 @@
+# preparation -------------------------------------------------------------
 rm(list = ls())
 
-# preparation -------------------------------------------------------------
-
-# load packages
 library(readxl)
+
+getname <- function(object){
+  deparse(substitute(object))
+  }
 
 # import models 
 model_files <- list.files(path='code/models', pattern='simulate', full.names = T)
@@ -11,68 +13,83 @@ lapply(model_files, source)
 
 # simulation settings ----------------------------------------------------------------
 
-# problem sets
-problems <- as.data.frame(read_xlsx("data/choice_problems_general.xlsx"))
-#problems <- as.data.frame(read_rds("data/choice_problems.rds"))
-
-# parameter sets
-# ...
-
 # number of synthetic agents
 n_agents <- 2 #1e3
+
+# problems
+SR_large <-  as.data.frame(read_xlsx("data/problems/SR_large_sys.xlsx"))
+SR_small <- as.data.frame(read_xlsx("data/problems/SR_small_sys.xlsx"))
+RR <-  as.data.frame(read_rds("data/problems/RR_large_rand.rds"))
+
+# parameters
+psi <- seq(.1, 1, .1) # switch rate
+theta_summary <- seq(100, 300, 100) # threshold summary comparison
+theta_roundwise <- 1:3 # thresholds roundwise comparison
+
+# when switch rates should decrease over time, replace psi by:
+base <- seq(.2,.8,.2) # minimum switch rate
+rate <- seq(.1, .5, .2) # rate of change
+
+summary_param_constant <- expand.grid(psi=psi, theta=theta_summary)
+roundwise_param_constant <- expand.grid(psi=psi, theta=theta_roundwise)
+
+summary_param_decreasing <- expand.grid(base=base, rate=rate, theta=theta_summary)
+roundwise_param_decreasing <- expand.grid(base=base, rate=rate, theta=theta_roundwise)
 
 # Constant switch rate (original) -------------------------------------------------------------
 
 ## safe-risky  -------------------------------------------------------------
 
-
 ### large EV differences ----------------------------------------------------
 
+# summary comparison
+set.seed(8172) # seed random number generator to make simulations reproducible
+simulation_summary_SR_large <- simulate_summary(problems=SR_large, param=summary_param_constant, n_agents)
+#checksum_simulation_summary <- digest(simulation_summary, "sha256")
+write_rds(simulation_summary_SR_large, "data/simulations/simulation_summary_SR_large.rds.bz2", compress = "bz2")
 
+# roundwise comparison
+set.seed(9821)
+simulation_roundwise_SR_large <- simulate_roundwise(problems=SR_large, param=roundwise_param_constant, n_agents)
+#checksum_simulation_roundwise <- digest(simulation_roundwise, "sha256")
+write_rds(simulation_roundwise_SR_large, "data/simulations/simulation_roundwise_SR_large.rds.bz2", compress = "bz2")
 
 ### small EV differences ----------------------------------------------------
 
-#### summary comparison ------------------------------------------------------
-# specify parameters for search rule (psi; switch rate) and stopping rule (theta; threshold)
-param <- expand.grid(psi = seq(.1, 1, .1) , theta = seq(100, 300, 50))
+# summary comparison
 set.seed(36151) # seed random number generator to make simulations reproducible
-simulation_summary <- simulate_summary(problems,param,n_agents)
-# checksum_simulation_summary <- digest(simulation_summary, "sha256")
-#write_rds(simulation_summary, "data/simulation_summary.rds.bz2", compress = "bz2")
+simulation_summary_SR_small <- simulate_summary(problems=SR_small, param=summary_param_constant, n_agents)
+#checksum_simulation_summary <- digest(simulation_summary, "sha256")
+write_rds(simulation_summary_SR_small, "data/simulations/simulation_summary_SR_small.rds.bz2", compress = "bz2")
 
-#### roundwise comparison ----------------------------------------------------
-param <- expand.grid(psi = seq(.1, 1, .1), theta = 1:5)
+# roundwise comparison
 set.seed(6535)
-simulation_roundwise <- simulate_roundwise(problems,param,n_agents)
-# checksum_simulation_roundwise <- digest(simulation_roundwise, "sha256")
-write_rds(simulation_roundwise, "data/simulation_roundwise.rds.bz2", compress = "bz2")
-
+simulation_roundwise_SR_small <- simulate_roundwise(problems=SR_small, param=roundwise_param_constant, n_agents)
+#checksum_simulation_roundwise <- digest(simulation_roundwise, "sha256")
+write_rds(simulation_roundwise_SR_small, "data/simulations/simulation_roundwise_SR_small.rds.bz2", compress = "bz2")
 
 
 ## risky-risky -------------------------------------------------------------
+# we use large EV differences to demonstrate the effect of problems with only risky gambles
 
+set.seed(5612) # seed random number generator to make simulations reproducible
+simulation_summary_RR <- simulate_summary(problems=RR, param=summary_param_constant, n_agents)
+#checksum_simulation_summary <- digest(simulation_summary, "sha256")
+write_rds(simulation_summary_RR, "data/simulations/simulation_summary_RR.rds.bz2", compress = "bz2")
 
-### large EV differences ----------------------------------------------------
-
-
-
-### small EV differences ----------------------------------------------------
-
-
+# roundwise comparison
+set.seed(9371)
+simulation_roundwise_RR <- simulate_roundwise(problems=RR, param=roundwise_param_constant, n_agents)
+#checksum_simulation_roundwise <- digest(simulation_roundwise, "sha256")
+write_rds(simulation_roundwise, "data/simulations/simulation_roundwise_RR.rds.bz2", compress = "bz2")
 
 
 # Decreasing switch rate --------------------------------------------------
 # we use safe-risky problems with large EV differences to demonstrate the effect of decreasing switch rates
 
-param <- expand.grid(base = seq(.2,.8,.2) , 
-                     rate = seq(.1, .5, .2) ,
-                     theta = seq(100, 300, 100))
-simulation_summary_decreasing <- simulate_summary_decreasing(problems, param=param, n_agents )
-#write_rds(simulation_summary, 'revision/data/simulation_summary_original_decreasing.rds')
+simulation_summary_decreasing <- simulate_summary_decreasing(problems=SR_large, param=summary_param_decreasing, n_agents)
+#write_rds(simulation_summary_decreasing, 'data/simulations/simulation_summary_decreasing.rds.bz2', compress = "bz2")
 
 # roundwise
-param <- expand.grid(base = seq(.2,.8,.2) , 
-                     rate = seq(.1, .5, .2) ,
-                     theta = 1:3)
-simulation_roundwise_decreasing <- simulate_roundwise_decreasing(problems, param=param, n_agents )
-write_rds(simulation_roundwise, 'revision/data/simulation_roundwise_original_decreasing.rds')
+simulation_roundwise_decreasing <- simulate_roundwise_decreasing(problems=SR_large, param=roundwise_param_decreasing, n_agents)
+#write_rds(simulation_roundwise, 'data/simulations/simulation_roundwise_decreasing.rds.bz2', compress = "bz2")
